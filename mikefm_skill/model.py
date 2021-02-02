7d651de0bea93c20162e80cc5a89c8ed5aa0dd01
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-02-02 11:28:06 +0100

    Update model.py

--- mikefm_skill/model.py
+++ mikefm_skill/model.py
@@ -2,4 +2,25 @@ import numpy as np
 from mikeio import Dfs0, Dfsu
 
 class ModelResult:
-    pass
+    filename = None
+    dfs = None
+    observations = None
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.dfs = Dfsu(filename)
+
+    def __repr__(self):
+        out = []
+        out.append("ModelResult")
+        out.append(self.filename)
+        return str.join("\n", out)
+
+    def add_observation(self, observation):
+        pass
+
+    def is_observation_in_domain(self, observation):
+        pass
+
+    def extract(self, observation):
+        pass
