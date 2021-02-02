0acad6ffc34f97c3254ec12a3fbd9022b7abd05f
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-02-02 11:57:26 +0100

    name and more

--- mikefm_skill/observation.py
+++ mikefm_skill/observation.py
@@ -3,23 +3,25 @@ from mikeio import Dfs0
 
 class PointObservation:
     
+    name = None
     filename = None
     dfs = None
     x = None
     y = None
     z = None
-    item = None
+    item_number = None
     
-    def __init__(self, filename, x, y, z=None):
+    def __init__(self, filename, x, y, z=None, item=0, name=None):
         self.filename = filename
         self.dfs = Dfs0(filename)
         self.x = x
         self.y = y
         self.z = z
 
-    def __repr__(self):
-        out = []
-        out.append("PointObservation")
-        out.append(self.filename)
-        return str.join("\n", out)
+        if name is None:
+            name = filename
+        self.name = name
 
+    def __repr__(self):
+        out = f"PointObservation: {self.name}"
+        return out
