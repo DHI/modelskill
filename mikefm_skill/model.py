6cce7937fabd9abf097031c33caee474cf7915c9
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-02-02 15:03:26 +0100

    ModelResultPoint and extract

--- mikefm_skill/model.py
+++ mikefm_skill/model.py
@@ -7,21 +7,25 @@ class ModelResult:
     filename = None
     dfs = None
     observations = []
+    items = []
 
     def __init__(self, filename):
+        # add "start" as user may wish to disregard start from comparison
         self.filename = filename
         self.dfs = Dfsu(filename)
 
     def __repr__(self):
+        #return self.dfs
         out = []
         out.append("ModelResult")
         out.append(self.filename)
         return str.join("\n", out)
 
-    def add_observation(self, observation):
+    def add_observation(self, observation, item):
         ok = self._validate_observation(observation)
         if ok:
             self.observations.append(observation)
+            self.items.append(item)
         else:
             warnings.warn('Could not add observation')
 
@@ -29,4 +33,33 @@ class ModelResult:
         return self.dfs.contains([observation.x, observation.y])
 
     def extract(self):
+        """extract model result in observation positions
+        """
+        ex = []
+        for j, obs in enumerate(self.observations):
+            mrp = self._extract_point_observation(obs, self.items[j])
+            ex.append(mrp)
+        return ex
+
+    def _extract_point_observation(self, observation, item):
+        elemids, _ = self.dfs.get_2d_interpolant([observation.x, observation.y], n_nearest=1)
+        modeldata = self.dfs.read(elements=elemids, items=[item])
+        return ModelResultPoint(observation, modeldata)
+
+
+
+class ModelResultPoint:
+    observation = None
+    model = None
+    time = None
+    data = None
+
+    def __init__(self, observation, data):
+        self.observation = observation
+        self.data = data
+
+    def plot(self):
+        pass
+
+    def statistics(self):
         pass
