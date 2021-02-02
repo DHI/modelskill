5315a50ead6f0ad3833af64a3adb8af980f0d23a
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-02-02 11:28:10 +0100

    Update observation.py

--- mikefm_skill/observation.py
+++ mikefm_skill/observation.py
@@ -2,6 +2,24 @@ import numpy as np
 from mikeio import Dfs0
 
 class PointObservation:
-    pass
+    
+    filename = None
+    dfs = None
+    x = None
+    y = None
+    z = None
+    item = None
+    
+    def __init__(self, filename, x, y, z=None):
+        self.filename = filename
+        self.dfs = Dfs0(filename)
+        self.x = x
+        self.y = y
+        self.z = z
 
+    def __repr__(self):
+        out = []
+        out.append("PointObservation")
+        out.append(self.filename)
+        return str.join("\n", out)
 
