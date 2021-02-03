ccd60f1caacf4825fa29c225747c505667dbd576
Henrik Andersson <jan@dhigroup.com>
2021-02-03 09:01:33 +0100

    More metrics

--- mikefm_skill/metrics.py
+++ mikefm_skill/metrics.py
@@ -1,11 +1,18 @@
 
 import numpy as np
 
+def bias(obs, model):
+    "Bias (model - obs)"
+    return np.mean(model - obs)
+
 def mean_absolute_error(obs: np.ndarray, model: np.ndarray, weights: np.ndarray=None):
     """Mean Absolute Error (MAE)"""
 
-    error = np.average(np.abs(obs - model), weights=weights)
+    error = np.average(np.abs(model - obs), weights=weights)
     return error
+
+def rmse(obs: np.ndarray, model: np.ndarray, weights: np.ndarray=None):
+    return root_mean_squared_error(obs, model, weights)
 
 def root_mean_squared_error(obs: np.ndarray, model: np.ndarray, weights: np.ndarray=None):
     """Root Mean Squared Error (RMSE)"""
@@ -22,3 +29,11 @@ def nash_sutcliffe_efficiency(obs, model):
     )
 
     return error
+
+def corr_coef(obs, model):
+
+    return np.corrcoef(obs, model)[0,1]
+
+def scatter_index(obs, model):
+
+    return np.sqrt( np.sum(((model - model.mean()) - (obs - obs.mean()))**2) / np.sum(obs**2) )
