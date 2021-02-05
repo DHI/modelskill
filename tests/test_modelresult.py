f7b25c48220529559fc1c1a2bbd45af99d194d12
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-02-05 22:59:25 +0100

    Update test_modelresult.py

--- tests/test_modelresult.py
+++ tests/test_modelresult.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mikefm_skill.model import ModelResult
+from mikefm_skill.model import ModelResult, ModelResultType
 from mikefm_skill.observation import PointObservation
 
 
@@ -23,21 +23,56 @@ def drogden():
 
 
 @pytest.fixture
-def oresund_2d():
+def hd_oresund_2d():
     return "tests/testdata/Oresund2D.dfsu"
 
 
-def test_dfs_object(oresund_2d):
-    mr = ModelResult(oresund_2d)
+@pytest.fixture
+def Hm0_HKNA():
+    fn = "tests/testdata/SW/HKNA_Hm0.dfs0"
+    return PointObservation(fn, item=0, x=4.2420, y=52.6887, name="HKNA")
+
+
+@pytest.fixture
+def Hm0_EPL():
+    fn = "tests/testdata/SW/eur_Hm0.dfs0"
+    return PointObservation(fn, item=0, x=3.2760, y=51.9990, name="EPL")
+
+
+@pytest.fixture
+def sw_dutch_coast():
+    return "tests/testdata/SW/HKZN_local_2017_DutchCoast.dfsu"
+
+
+def test_dfs_object(hd_oresund_2d):
+    mr = ModelResult(hd_oresund_2d)
 
     assert mr.dfs.is_2d
 
 
-def test_extract(oresund_2d, klagshamn, drogden):
-    mr = ModelResult(oresund_2d)
+def test_ModelResultType(sw_dutch_coast):
+    mr = ModelResult(sw_dutch_coast)
+
+    assert mr.type == ModelResultType.dfsu
+
+
+def test_ModelResultType0():
+    mr = ModelResult("tests/testdata/TS.dfs0")
+
+    assert mr.type == ModelResultType.dfs0
+
+
+def test_extract(hd_oresund_2d, klagshamn, drogden):
+    mr = ModelResult(hd_oresund_2d)
 
     mr.add_observation(klagshamn, item=0)
     mr.add_observation(drogden, item=0)
     collection = mr.extract()
     collection[0].name == "Klagshamn"
 
+
+def test_plot_positions(sw_dutch_coast, Hm0_EPL, Hm0_HKNA):
+    mr = ModelResult(sw_dutch_coast)
+    mr.add_observation(Hm0_EPL, item=0)
+    mr.add_observation(Hm0_HKNA, item=0)
+    mr.plot_observation_positions()
