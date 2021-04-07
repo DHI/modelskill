58f8fd026a6680633f1285655ea5c62aeeeb7fb2
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-04-07 08:59:30 +0200

    more tests

--- tests/test_multimodelcompare.py
+++ tests/test_multimodelcompare.py
@@ -1,7 +1,7 @@
 import pytest
 
 from fmskill.model import ModelResult, ModelResultCollection
-from fmskill.observation import PointObservation
+from fmskill.observation import PointObservation, TrackObservation
 from fmskill.metrics import root_mean_squared_error, mean_absolute_error
 
 
@@ -30,7 +30,13 @@ def o2():
 
 
 @pytest.fixture
-def mrc(mr1, mr2, o1, o2):
+def o3():
+    fn = "tests/testdata/SW/Alti_c2_Dutch.dfs0"
+    return TrackObservation(fn, item=3, name="c2")
+
+
+@pytest.fixture
+def mrc(mr1, mr2):
     return ModelResultCollection([mr1, mr2])
 
 
@@ -40,6 +46,15 @@ def mrc_o1o2(mr1, mr2, o1, o2):
     mrc.add_observation(o1, item=0)
     mrc.add_observation(o2, item=0)
     return mrc
+
+
+@pytest.fixture
+def cc(mr1, mr2, o1, o2, o3):
+    mrc = ModelResultCollection([mr1, mr2])
+    mrc.add_observation(o1, item=0)
+    mrc.add_observation(o2, item=0)
+    mrc.add_observation(o3, item=0)
+    return mrc.extract()
 
 
 def test_mrc_repr(mrc):
@@ -60,3 +75,10 @@ def test_add_observation(mrc, o1):
 def test_extract(mrc_o1o2):
     cc = mrc_o1o2.extract()
     assert True
+
+
+def test_mm_skill(cc):
+    df = cc.skill(model="SW_1")
+    assert df.loc["EPL"].n == 66
+    assert df.loc["c2"].n == 113
+
