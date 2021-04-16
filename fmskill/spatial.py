01505e20b0d919bb024dd493afbb7589b48d9e91
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-04-16 14:54:49 +0200

    properties and simple plot

--- fmskill/spatial.py
+++ fmskill/spatial.py
@@ -2,9 +2,29 @@ import numpy as np
 
 
 class SpatialSkill:
+    @property
+    def x(self):
+        return self.ds.x
+
+    @property
+    def y(self):
+        return self.ds.y
+
+    @property
+    def coords(self):
+        return self.ds.coords
+
+    @property
+    def n(self):
+        if "n" in self.ds:
+            return self.ds.n
+
     def __init__(self, ds, name: str = None):
         self.ds = ds
         self.name = name
 
-    def plot(self, **kwargs):
-        return self.ds.plot(**kwargs)
+    def plot(self, field, **kwargs):
+        return self.ds[field].plot(**kwargs)
+
+    def to_dataframe(self):
+        return self.ds.to_dataframe()
