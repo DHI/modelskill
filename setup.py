d9a17e3b8e7532ecf1b5c0d686a5821f6f8641c1
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-04-20 20:55:23 +0200

    black version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
             "pytest",
             "sphinx",
             "sphinx-rtd-theme",
-            "black",
+            "black >= 20.7",
             "shapely",
             "plotly >= 4.5",
         ],
