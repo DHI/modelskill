f15cd5e8cf79c844c13c731a79e24c9218809a82
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-04-20 20:58:39 +0200

    prepare release 0.2.0

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("LICENSE") as fh:
 
 setuptools.setup(
     name="fmskill",
-    version="0.1.2.dev",
+    version="0.2.0",
     install_requires=["numpy", "pandas", "mikeio >= 0.6", "matplotlib", "xarray"],
     extras_require={
         "dev": [
             "pytest",
             "sphinx",
             "sphinx-rtd-theme",
-            "black >= 20.7",
+            "black==20.8b1",
             "shapely",
             "plotly >= 4.5",
         ],
