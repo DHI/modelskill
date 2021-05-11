c6ec692f22732beffeab25a36b069a91303377e0
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-11 12:38:02 +0200

    Iterable not string

--- fmskill/compare.py
+++ fmskill/compare.py
@@ -564,7 +564,7 @@ class BaseComparer:
         """
 
         metrics = self._parse_metric(metrics)
-        if not isinstance(metrics, List):
+        if callable(metrics) or isinstance(metrics, str):
             metrics = [metrics]
 
         df = self.sel_df(
@@ -582,7 +582,7 @@ class BaseComparer:
         n_models = len(df.model.unique())
         n_obs = len(df.observation.unique())
         by = self._parse_by(by, n_models, n_obs)
-        if not isinstance(by, List):
+        if isinstance(by, str) or (not isinstance(by, Iterable)):
             by = [by]
         if not "x" in by:
             by.insert(0, "x")
