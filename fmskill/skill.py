e99e967b69fcbe605446779909df3e810c042d2c
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-14 11:28:00 +0200

    SkillDataFrame class, obs_name, mod_names and more

--- fmskill/skill.py
+++ fmskill/skill.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.plotting import parallel_coordinates
 
 # import numpy as np
 # import warnings
@@ -6,72 +7,93 @@ import pandas as pd
 # from IPython.display import display
 
 
-class BaseSkill(pd.DataFrame):
-    # ALTERNATIVE approach: where the class inherits from DataFrame
-    # instead of holding a DataFrame as an attribute.
-
-    # https://pandas.pydata.org/pandas-docs/stable/development/extending.html#extending-subclassing-pandas
-    # https://stackoverflow.com/questions/47466255/subclassing-a-pandas-dataframe-updates
-    # This class variable tells Pandas the name of the attributes
-    # that are to be ported over to derivative DataFrames.  There
-    # is a method named `__finalize__` that grabs these attributes
-    # and assigns them to newly created `BaseSkill`
-    _metadata = ["my_attr"]
-
-    @property
-    def _constructor(self):
-        """This is the key to letting Pandas know how to keep
-        derivative `BaseSkill` the same type as yours.  It should
-        be enough to return the name of the Class.  However, in
-        some cases, `__finalize__` is not called and `my_attr` is
-        not carried over.  We can fix that by constructing a callable
-        that makes sure to call `__finalize__` every time."""
-
-        def _c(*args, **kwargs):
-            return BaseSkill(*args, **kwargs).__finalize__(self)
-
-        return _c
-
-    def __init__(self, *args, **kwargs):
-        # grab the keyword argument that is supposed to be my_attr
-        self.my_attr = kwargs.pop("my_attr", None)
-        super().__init__(*args, **kwargs)
-
-    # def my_method(self, other):
-    #     return self * np.sign(self - other)
-
-
-class AggregatedSkill:
+class SkillDataFrame:
     def __init__(self, df):
-        # super().__init__()
         self.df = df
 
     def __repr__(self):
-        #     display(self.df)
         return repr(self.df)
+
+    def _repr_html_(self):
+        return self.df._repr_html_()
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, x):
-        # if isinstance(x, int):
-        #     x = self._get_obs_name(x)
         return self.df[x]
 
-    def _repr_html_(self):
-        return self.df._repr_html_()
+    @property
+    def loc(self, *args, **kwargs):
+        return self.df.loc(*args, **kwargs)
 
+    @property
+    def iloc(self, *args, **kwargs):
+        return self.df.iloc(*args, **kwargs)
+
+    @property
     def index(self):
-        return self.df.index()
+        return self.df.index
 
-    def head(self, **kwargs):
-        return AggregatedSkill(self.df.head(**kwargs))
+    @property
+    def shape(self):
+        return self.df.shape
 
-    def round(self, precision):
-        return AggregatedSkill(self.df.round(precision))
+    @property
+    def size(self):
+        return self.df.size
 
-    def sort_values(self, field, **kwargs):
-        return AggregatedSkill(self.df.sort_values(field, **kwargs))
+    @property
+    def ndims(self):
+        return self.df.ndims
+
+    def to_html(self, *args, **kwargs):
+        return self.df.to_html(*args, **kwargs)
+
+    def to_dataframe(self, copy=True):
+        if copy:
+            return self.df.copy()
+        else:
+            return self.df
+
+    def head(self, *args, **kwargs):
+        return self.__class__(self.df.head(*args, **kwargs))
+
+    def tail(self, *args, **kwargs):
+        return self.__class__(self.df.tail(*args, **kwargs))
+
+    def round(self, decimals, *args, **kwargs):
+        return self.__class__(self.df.round(decimals))
+
+    def sort_values(self, by, **kwargs):
+        return self.__class__(self.df.sort_values(by, **kwargs))
+
+
+class AggregatedSkill(SkillDataFrame):
+    @property
+    def mod_names(self):
+        return self._get_index_level_by_name("model")
+
+    @property
+    def obs_names(self):
+        return self._get_index_level_by_name("observation")
+
+    def _get_index_level_by_name(self, name):
+        if name in self.index.names:
+            level = self.index.names.index(name)
+            return self.index.get_level_values(level).unique()
+        else:
+            return []
+            # raise ValueError(f"name {name} not in index {list(self.index.names)}")
+
+    def parallel_coordinates(self):
+        pass
+
+    def plot_bar(self):
+        pass
+
+    def plot_grid(self):
+        pass
 
     def style(self, precision=3, columns=None, cmap="Reds", background_gradient=True):
         float_list = ["float16", "float32", "float64"]
@@ -131,7 +153,6 @@ class AggregatedSkill:
         styled_df = styled_df.apply(self._style_one_best, subset=cols)
         # cols = list(set(zero_is_good_metrics) & set(float_cols.append("bias")))
         if "bias" in float_cols:
-            print("best bias")
             styled_df = styled_df.apply(self._style_abs_min, subset=["bias"])
         # , subset=
 
@@ -168,11 +189,5 @@ class AggregatedSkill:
         return [cell_style if v else "" for v in (s == s.max())]
         # font-weight: bold; font-style: oblique; border-style: solid; border-color: #212121; border-style: solid; border-width: thin;
 
-    def taylor_diagram(self):
-        raise NotImplementedError()
-
     def target_diagram(self):
         raise NotImplementedError()
-
-    def to_html(self, **kwargs):
-        return self.df.to_html(**kwargs)
