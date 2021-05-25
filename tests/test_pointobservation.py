5bdf519841ab92586d8189564d30050a7e496080
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-25 16:20:51 +0200

    test from df

--- tests/test_pointobservation.py
+++ tests/test_pointobservation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 from shapely.geometry import Point
@@ -12,3 +13,17 @@ def klagshamn():
 def test_coordinates(klagshamn):
     o1 = PointObservation(klagshamn, item=0, x=366844, y=6154291, name="Klagshamn")
     assert isinstance(o1.geometry, Point)
+
+
+def test_from_df(klagshamn):
+    o1 = PointObservation(klagshamn, item=0, x=366844, y=6154291, name="Klagshamn1")
+
+    df = o1.df
+    assert isinstance(df, pd.DataFrame)
+    o2 = PointObservation(df, item=0, x=366844, y=6154291, name="Klagshamn2")
+    assert o1.n_points == o2.n_points
+
+    s = o1.df["Water Level"]
+    assert isinstance(s, pd.Series)
+    o3 = PointObservation(s, x=366844, y=6154291, name="Klagshamn2")
+    assert o1.n_points == o3.n_points
