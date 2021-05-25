a251475fd4c781b7a034f71d89aadadb61fd71e2
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-25 16:48:25 +0200

    better tests

--- tests/test_pointobservation.py
+++ tests/test_pointobservation.py
@@ -10,9 +10,12 @@ def klagshamn():
     return "tests/testdata/smhi_2095_klagshamn.dfs0"
 
 
-def test_coordinates(klagshamn):
+def test_from_dfs0(klagshamn):
     o1 = PointObservation(klagshamn, item=0, x=366844, y=6154291, name="Klagshamn")
-    assert isinstance(o1.geometry, Point)
+    assert o1.n_points == 50328
+
+    o2 = PointObservation(klagshamn, item="Water Level", x=366844, y=6154291)
+    assert o1.n_points == o2.n_points
 
 
 def test_from_df(klagshamn):
@@ -23,7 +26,19 @@ def test_from_df(klagshamn):
     o2 = PointObservation(df, item=0, x=366844, y=6154291, name="Klagshamn2")
     assert o1.n_points == o2.n_points
 
+    # item as str
+    o2 = PointObservation(df, item="Water Level", x=366844, y=6154291)
+    assert o1.n_points == o2.n_points
+
     s = o1.df["Water Level"]
     assert isinstance(s, pd.Series)
-    o3 = PointObservation(s, x=366844, y=6154291, name="Klagshamn2")
+    o3 = PointObservation(s, x=366844, y=6154291, name="Klagshamn3")
     assert o1.n_points == o3.n_points
+
+
+def test_coordinates(klagshamn):
+    x0, y0 = 366844, 6154291
+    o1 = PointObservation(klagshamn, item=0, x=x0, y=y0, name="Klagshamn")
+    assert isinstance(o1.geometry, Point)
+    assert o1.geometry.x == x0
+    assert o1.geometry.y == y0
