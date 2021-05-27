fcc00a7b5c262b94478f955fcc5c3e8ca74321cd
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-27 09:56:59 +0200

    more tests

--- tests/test_simple_compare.py
+++ tests/test_simple_compare.py
@@ -32,13 +32,20 @@ def test_compare_df(fn_mod, fn_obs):
     assert c.n_points == 66
 
 
+def test_compare_point_obs(fn_mod, fn_obs):
+    df_mod = Dfs0(fn_mod).read(items=0).to_dataframe()
+    obs = fmskill.PointObservation(fn_obs, name="EPL")
+    c = fmskill.compare(df_mod, obs)
+    assert c.n_points == 66
+
+
 def test_compare_fail(fn_mod, fn_obs):
     df_mod = Dfs0(fn_mod).read(items=[0, 1, 2]).to_dataframe()
     with pytest.raises(ValueError):
-        # multiple items in model df
+        # multiple items in model df -> ambigous
         c = fmskill.compare(df_mod, fn_obs)
 
     df_obs2, fn_mod2 = df_mod, fn_obs
     with pytest.raises(ValueError):
-        # multiple items in obs df
+        # multiple items in obs df -> ambigous
         c = fmskill.compare(fn_mod2, df_obs2)
