58f5fafb59bce6b0a9cdb0a76c00bcbf4586c57a
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-06-04 14:05:18 +0200

    Create test_model_xarray.py

---
+++ tests/test_model_xarray.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+import pytest
+
+from fmskill.model import ModelResult
+from fmskill.model.abstract import ModelResultInterface
+from fmskill.model import XArrayModelResult, XArrayModelResultItem
+from fmskill.observation import PointObservation
