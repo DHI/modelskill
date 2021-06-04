8b046b109030d75957e3d6f7ca8731a6369726a2
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-06-04 14:05:09 +0200

    add XArrayModelResult, XArrayModelResultItem

--- fmskill/model/__init__.py
+++ fmskill/model/__init__.py
@@ -1,3 +1,4 @@
 from .factory import ModelResult
 from .dfs import DfsModelResult, DfsModelResultItem
 from .pandas import DataFrameModelResult, DataFrameModelResultItem
+from .xarray import XArrayModelResult, XArrayModelResultItem
