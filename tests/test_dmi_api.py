2912ccf715b49167096e74db27df571a15d19b56
Henrik Andersson <jan@dhigroup.com>
2021-06-09 09:17:30 +0200

    Renamed n to limit

--- tests/test_dmi_api.py
+++ tests/test_dmi_api.py
@@ -7,16 +7,27 @@ from fmskill.data.dmi import DMIOceanObsRepository
 
 def test_get_observations():
 
-    if "DMI_API_KEY" not in os.environ:
-        warnings.warn("Environment variable 'DMI_API_KEY' not present, skipping test")
-
     api_key = os.environ["DMI_API_KEY"]
     repo = DMIOceanObsRepository(apikey=api_key)
 
     station_id = "30336"  # Kbh havn
 
     df = repo.get_observations(
-        station_id=station_id, start_time=datetime(2020, 1, 1), n=100
+        station_id=station_id, start_time=datetime(2020, 1, 1), limit=100
     )
 
     assert df.shape[0] > 0
+
+
+def test_get_stations():
+    api_key = os.environ["DMI_API_KEY"]
+    repo = DMIOceanObsRepository(apikey=api_key)
+
+    assert repo.stations.shape[0] > 0
+
+    cols = repo.stations.columns
+
+    assert "station_id" in cols
+    assert "lon" in cols
+    assert "lat" in cols
+    assert "name" in cols
