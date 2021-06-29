97ba7011140f23bab67e99dcaa8be9a257ffa460
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-06-29 14:46:47 +0000

    test overlaps

--- tests/test_concatenate.py
+++ tests/test_concatenate.py
@@ -51,22 +51,16 @@ def o123():
     return o1, o2, o3
 
 
-# @pytest.fixture
-# def cc_mike(o123, mrmike):
-#     con = Connector([*o123], mrmike)
-#     return con.extract()
-
-
 def test_concat_time(o123, mr28, mr29, mr2days):
-    con1 = Connector([*o123], mr28)
+    con1 = Connector(o123, mr28)
     cc1 = con1.extract()
-    con2 = Connector([*o123], mr29)
+    con2 = Connector(o123, mr29)
     cc2 = con2.extract()
 
     # Note: the multi-file dataset will have interpolated values
     # between 23:00 the first day and 00:00 the second day
     # that is NOT the case with the concatenated cc12b
-    con12 = Connector([*o123], mr2days)
+    con12 = Connector(o123, mr2days)
     cc12 = con12.extract()
 
     assert cc1.start == cc12.start
@@ -76,15 +70,16 @@ def test_concat_time(o123, mr28, mr29, mr2days):
     assert cc1.start == cc12b.start
     assert cc2.end == cc12b.end
     assert cc12b.n_points < cc12.n_points
+    assert cc12b.n_points == cc1.n_points + cc2.n_points
 
 
 def test_concat_model(o123, mrmike, mrmike2):
-    con1 = Connector([*o123], mrmike)
+    con1 = Connector(o123, mrmike)
     cc1 = con1.extract()
-    con2 = Connector([*o123], mrmike2)
+    con2 = Connector(o123, mrmike2)
     cc2 = con2.extract()
 
-    con12 = Connector([*o123], [mrmike, mrmike2])
+    con12 = Connector(o123, [mrmike, mrmike2])
     cc12 = con12.extract()
 
     assert len(cc1.mod_names) == 1
@@ -99,13 +94,13 @@ def test_concat_model(o123, mrmike, mrmike2):
 
 
 def test_concat_model_different_time(o123, mrmike, mr2days):
-    con1 = Connector([*o123], mrmike)
+    con1 = Connector(o123, mrmike)
     cc1 = con1.extract()
-    con2 = Connector([*o123], mr2days)
+    con2 = Connector(o123, mr2days)
     cc2 = con2.extract()
 
-    # note the times coverage is different for the two models
-    con12 = Connector([*o123], [mrmike, mr2days])
+    # note the time coverage is different for the two models
+    con12 = Connector(o123, [mrmike, mr2days])
     cc12 = con12.extract()
 
     assert len(cc1.mod_names) == 1
@@ -113,3 +108,67 @@ def test_concat_model_different_time(o123, mrmike, mr2days):
     assert cc1.mod_names[0] == cc12.mod_names[0]
     assert cc2.mod_names[0] == cc12.mod_names[-1]
     assert cc2.end == cc12.end
+
+    cc12b = cc1 + cc2
+    assert cc12b.score() == cc12.score()
+    assert cc12b.n_points == cc12.n_points
+
+
+def test_concat_same_model(o123, mrmike):
+    con1 = Connector(o123, mrmike)
+    cc1 = con1.extract()
+    con2 = Connector(o123, mrmike)
+    cc2 = con2.extract()
+
+    # if we add the same model multiple times it has no effect
+    cc12 = cc1 + cc2
+    assert cc12.n_points == cc1.n_points
+    assert cc12[0].df.index.is_unique
+    assert cc1.score() == cc12.score()
+
+
+def test_concat_time_overlap(o123, mrmike):
+    con1 = Connector(o123, mrmike)
+    cc1 = con1.extract()
+
+    # if there they don't cover the same period...
+    o1 = o123[0].copy()
+    o1.df = o1.df["2017-10-26":"2017-10-27"]
+
+    o2 = o123[1].copy()
+    o2.df = o2.df["2017-10-26":"2017-10-27"]
+
+    o3 = o123[2].copy()
+    o3.df = o3.df["2017-10-26":"2017-10-27"]
+
+    con26 = Connector([o1, o2, o3], mrmike)
+    cc26 = con26.extract()
+
+    assert cc1.start == cc26.start
+    assert cc1.end > cc26.end
+    assert cc1.n_points > cc26.n_points
+
+    # cc26 completely contained in cc1
+    cc12 = cc1 + cc26
+    assert cc1.start == cc12.start
+    assert cc1.end == cc12.end
+    assert cc1.n_points == cc12.n_points
+    assert cc1.score() == cc12.score()
+
+    o1 = o123[0].copy()
+    o1.df = o1.df["2017-10-27 12:00":"2017-10-29 23:00"]
+
+    o2 = o123[1].copy()
+    o2.df = o2.df["2017-10-27 12:00":"2017-10-29 23:00"]
+
+    o3 = o123[2].copy()
+    o3.df = o3.df["2017-10-27 12:00":"2017-10-29 23:00"]
+
+    con2 = Connector([o1, o2, o3], mrmike)
+    cc2 = con2.extract()
+
+    # cc26 _not_ completely contained in cc2
+    cc12 = cc26 + cc2
+    assert cc2.start > cc12.start
+    assert cc2.end == cc12.end
+    assert cc2.n_points < cc12.n_points
