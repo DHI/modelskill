68f08d561500d0752796bcb9a27ba828a5a7b476
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-10-04 14:57:20 +0200

    add wilmott

--- fmskill/metrics.py
+++ fmskill/metrics.py
@@ -45,6 +45,8 @@ Examples
 0.637783218973691
 >>> lin_slope(obs, mod)
 0.4724896836313617
+>>> wilmott(obs, mod)
+0.7484604452865941
 >>> hit_ratio(obs, mod, a=0.5)
 0.6666666666666666
 """
@@ -378,6 +380,41 @@ def scatter_index(obs: np.ndarray, model: np.ndarray) -> float:
     )
 
 
+def wilmott(obs: np.ndarray, model: np.ndarray) -> float:
+    """Wilmott's Index of Agreement
+
+    A scaled representation of the predictive accuracy of the model against observations. A value of 1 indicates a perfect match, and 0 indicates no agreement at all.
+
+    .. math::
+
+        wilmott = 1 - \\frac{\\frac{1}{n} \\sum_{i=1}^n(model_i - obs_i)^2}
+                           {\\frac{1}{n} \\sum_{i=1}^n(|model_i - \\overline{obs}| + |obs_i - \\overline{obs}|)^2}
+
+    Range: 0.0 to 1.0; Best: 1.0
+
+    Examples
+    --------
+    >>> obs = np.array([1.0, 1.1, 1.2, 1.3, 1.4, 1.4, 1.3])
+    >>> model = np.array([1.02, 1.16, 1.3, 1.38, 1.49, 1.45, 1.32])
+    >>> wilmott(obs, model)
+    0.9501403174479723
+
+    References
+    ----------
+    Willmott, C. J. 1981. "On the validation of models". Physical Geography, 2, 184–194.
+    """
+
+    assert obs.size == model.size
+    if len(obs) == 0:
+        return np.nan
+
+    residual = model.ravel() - obs.ravel()
+    nominator = np.sum(residual ** 2)
+    denominator = np.sum((np.abs(model - obs.mean()) + np.abs(obs - obs.mean())) ** 2)
+
+    return 1 - nominator / denominator
+
+
 def hit_ratio(obs: np.ndarray, model: np.ndarray, a=0.1) -> float:
     """Fraction within obs ± acceptable deviation
 
@@ -389,8 +426,8 @@ def hit_ratio(obs: np.ndarray, model: np.ndarray, a=0.1) -> float:
 
     Examples
     --------
-    >>> obs = np.array([1.0,1.1,1.2,1.3,1.4, 1.4, 1.3])
-    >>> model = np.array([1.02, 1.16, 1.3 , 1.38, 1.49, 1.45, 1.32])
+    >>> obs = np.array([1.0, 1.1, 1.2, 1.3, 1.4, 1.4, 1.3])
+    >>> model = np.array([1.02, 1.16, 1.3, 1.38, 1.49, 1.45, 1.32])
     >>> hit_ratio(obs, model, a=0.05)
     0.2857142857142857
     >>> hit_ratio(obs, model, a=0.1)
