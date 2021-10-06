215ab4e52fce402a3025c630a6fd1c51cfd8a1d7
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-10-06 17:28:32 +0200

    change theme and add logo

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "fmskill"
+project = "FMskill"
 copyright = "2021, Jesper Mariegaard, Henrik Andersson, Hendrik Wrenger"
 author = "Jesper Mariegaard, Henrik Andersson, Hendrik Wrenger"
 
@@ -46,9 +46,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+html_logo = "../images/logo/mike-fm-skill-rgb.svg"
