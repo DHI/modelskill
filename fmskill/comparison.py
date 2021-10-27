cc6e84e1ee2ecea5d2b0dbde60fd9c815871a7bd
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-10-27 15:52:43 +0200

    repeat the right way

--- fmskill/comparison.py
+++ fmskill/comparison.py
@@ -1864,7 +1864,7 @@ class ComparerCollection(Mapping, Sequence, BaseComparer):
         # weights
         weights = self._parse_weights(weights, obs_names)
         skilldf["weights"] = (
-            skilldf.n if weights is None else np.repeat(weights, n_models)
+            skilldf.n if weights is None else np.tile(weights, n_models)
         )
         weighted_mean = lambda x: np.average(x, weights=skilldf.loc[x.index, "weights"])
 
