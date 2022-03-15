fe01148abdf452dd83fd5db338fb03cd597d130d
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2022-03-15 13:00:30 +0100

    Use alpha version of MIKE IO

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open("LICENSE") as fh:
 
 setuptools.setup(
     name="fmskill",
-    version="0.6.dev1",
+    version="0.6.dev2",
     install_requires=[
         "numpy",
         "pandas",
-        "mikeio >= 0.7",
+        "mikeio == 1.0a1",
         "matplotlib",
         "xarray",
         "markdown",
