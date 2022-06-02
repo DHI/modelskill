4e6776a7e9164b127f6b3a976a2db4652f43b3d3
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2022-06-02 12:04:34 +0200

    geometry.plot

--- fmskill/plot.py
+++ fmskill/plot.py
@@ -344,7 +344,10 @@ def scatter(
 
 
 def plot_observation_positions(
-    dfs: mikeio.dfsu._Dfsu, observations: List[Observation], figsize: Tuple = None, title=None
+    dfs: mikeio.dfsu._Dfsu,
+    observations: List[Observation],
+    figsize: Tuple = None,
+    title=None,
 ):
     """Plot observation points on a map showing the model domain
 
@@ -362,7 +365,7 @@ def plot_observation_positions(
 
     xn = dfs.node_coordinates[:, 0]
     offset_x = 0.02 * (max(xn) - min(xn))
-    ax = dfs.plot(plot_type="outline_only", figsize=figsize)
+    ax = dfs.geometry.plot(plot_type="outline_only", figsize=figsize)
     for obs in observations:
         if isinstance(obs, PointObservation):
             ax.scatter(x=obs.x, y=obs.y, marker="x")
