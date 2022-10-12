7a472f8243d7abc0230abe8ac50910907b7421b8
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2022-10-12 10:30:41 +0200

    normalize with abs mean

--- fmskill/metrics.py
+++ fmskill/metrics.py
@@ -410,13 +410,13 @@ def si(obs: np.ndarray, model: np.ndarray) -> float:
 def scatter_index(obs: np.ndarray, model: np.ndarray) -> float:
     """Scatter index (SI)
 
-    Which is the same as the unbiased-RMSE normalized by the mean of the observations. 
+    Which is the same as the unbiased-RMSE normalized by the absolute mean of the observations.
 
     .. math::
-        \\sqrt {\frac{1}{n} \\sum_{i=1}^n \\left( (model_i - \\overline {model}) - (obs_i - \\overline {obs}) \\right)^2
-        {\frac{1}{n} \\sum_{i=1}^n obs_i^2}}
+        \\frac{ \\sqrt{ \\frac{1}{n} \\sum_{i=1}^n \\left( (model_i - \\overline {model}) - (obs_i - \\overline {obs}) \\right)^2} }
+        {\\frac{1}{n} \\sum_{i=1}^n | obs_i | }
 
-    Range: [0, 100]; Best: 0
+    Range: [0, \\infty); Best: 0
     """
     assert obs.size == model.size
     if len(obs) == 0:
@@ -424,7 +424,7 @@ def scatter_index(obs: np.ndarray, model: np.ndarray) -> float:
 
     residual = obs.ravel() - model.ravel()
     residual = residual - residual.mean()  # unbiased
-    return np.sqrt(np.mean(residual**2)) / np.mean(obs.ravel())
+    return np.sqrt(np.mean(residual**2)) / np.mean(np.abs(obs.ravel()))
 
 
 def scatter_index2(obs: np.ndarray, model: np.ndarray) -> float:
