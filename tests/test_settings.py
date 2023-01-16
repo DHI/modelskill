7c1c71fa42377d95041912cb5d436e63514b7e63
Henrik Andersson <jan@dhigroup.com>
2023-01-16 14:38:11 +0100

    No ambiguous options

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,3 +1,5 @@
+import pytest
+
 import fmskill
 import fmskill.settings as settings
 
@@ -16,19 +18,25 @@ def test_options_repr():
     assert "metrics.list" in repr(o.metrics)
 
 
-def test_options_get_options():   
+def test_options_get_options():
     o = fmskill.options
     assert o.plot.scatter.points.size == 20
     assert fmskill.get_option("plot.scatter.points.size") == 20
-    
+
     # does not need to be complete, just unique
     assert fmskill.get_option("scatter.points.size") == 20
+
+    with pytest.raises(settings.OptionError):
+        # there are many color options
+        assert fmskill.get_option("color")
+
 
 def test_options_to_dict():
     o = fmskill.options
     d = o.to_dict()
     assert d["plot.scatter.points.size"] == 20
     assert d["metrics.list"][0] == o.metrics.list[0]
+
 
 def test_options_set_options():
     o = fmskill.options
@@ -39,7 +47,8 @@ def test_options_set_options():
 
     fmskill.set_option("plot.scatter.points.size", 200)
     assert o.plot.scatter.points.size == 200
-    
+
+
 def test_options_reset_options():
     o = fmskill.options
     o.plot.scatter.points.size = 300
