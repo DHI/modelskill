e001d5b6bdfbf903abee9942af436ac32041e5f7
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-01-16 17:26:28 +0100

    test register and getting dict valued options

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -31,6 +31,21 @@ def test_options_get_options():
         assert fmskill.get_option("color")
 
 
+def test_options_get_options_dict():
+    o = fmskill.options
+    d0 = o.plot.scatter.quantiles.kwargs
+    # dict is a special case, it cannot be accessed as an attribute
+    # without some additional code
+    assert not isinstance(d0, dict)
+    assert isinstance(d0, settings.OptionsContainer)
+    d1 = list(d0.to_dict().values())[0]
+    assert isinstance(d1, dict)
+    
+    # this is the way to get the dict
+    d = settings.get_option("plot.scatter.quantiles.kwargs")
+    assert isinstance(d, dict)
+
+
 def test_options_to_dict():
     o = fmskill.options
     d = o.to_dict()
@@ -48,6 +63,12 @@ def test_options_set_options():
     fmskill.set_option("plot.scatter.points.size", 200)
     assert o.plot.scatter.points.size == 200
 
+    # validation fails
+    with pytest.raises(ValueError):
+        fmskill.set_option("plot.scatter.points.size", "200")
+        fmskill.set_option("plot.scatter.points.size", -1)
+
+
 
 def test_options_reset_options():
     o = fmskill.options
@@ -55,3 +76,16 @@ def test_options_reset_options():
     assert o.plot.scatter.points.size == 300
     fmskill.reset_option("plot.scatter.points.size")
     assert o.plot.scatter.points.size == 20
+
+
+def test_options_register_option():
+    with pytest.raises(settings.OptionError):
+        # non-existing option
+        assert fmskill.get_option("plot.scatter.points.size2")
+    
+    settings.register_option("plot.scatter.points.size2", 200, "test")
+    assert fmskill.get_option("plot.scatter.points.size2") == 200
+    fmskill.set_option("plot.scatter.points.size2", 300)
+    assert fmskill.get_option("plot.scatter.points.size2") == 300
+    fmskill.reset_option("plot.scatter.points.size2")
+    assert fmskill.get_option("plot.scatter.points.size2") == 200
