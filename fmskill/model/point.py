79518cd5ea707d388d7083009bc33b360d68ff3d
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-03-12 22:10:18 +0100

    remove main and unused imports, parse input, WiP

--- fmskill/model/point.py
+++ fmskill/model/point.py
@@ -4,10 +4,7 @@ import mikeio
 import pandas as pd
 
 from fmskill import types, utils
-from fmskill.comparison import PointComparer, SingleObsComparer
-from fmskill.model import protocols
 from fmskill.model._base import ModelResultBase
-from fmskill.observation import PointObservation, TrackObservation
 
 
 class PointModelResult(ModelResultBase):
@@ -22,6 +19,18 @@ class PointModelResult(ModelResultBase):
         quantity: str = None,
     ) -> None:
 
+        if isinstance(data, (str, Path)):
+            assert Path(data).suffix == ".dfs0", "File must be a dfs0 file"
+            data = mikeio.read(data, items=item).to_dataframe()
+        elif isinstance(data, mikeio.Dfs0):
+            data = data.read(items=item).to_dataframe()
+        elif isinstance(data, mikeio.Dataset):
+            data = data.to_dataframe() if item is None else data[item].to_dataframe()
+        elif isinstance(data, mikeio.DataArray):
+            data = data.to_dataframe()
+        elif isinstance(data, pd.Series):
+            data = pd.DataFrame(data)  # to_frame?
+
         assert isinstance(
             data, get_args(types.PointType)
         ), "Could not construct PointModelResult from provided data"
@@ -30,31 +39,23 @@ class PointModelResult(ModelResultBase):
             data = mikeio.open(data)
 
         if isinstance(data, mikeio.Dfs0):
-            item, idx = utils.get_item_name_and_idx_dfs(data, item)
-            itemInfo = data.items[idx].type
+            item, idx = utils.get_item_name_and_idx_dfs(data.items, item)
+            if itemInfo is None:
+                itemInfo = data.items[idx]
             data = data.read()
             data = mikeio.Dataset(data[item]).to_dataframe().dropna()
+
+        # TODO: series
 
         if isinstance(data, pd.DataFrame):
             if data.empty or len(data.columns) == 0:
                 raise ValueError("No data.")
 
-            item, _ = utils.get_item_name_and_idx_pd(data, item)
+            item, _ = utils.get_item_name_and_idx(list(data.columns), item)
             data = data[[item]]
-            if itemInfo is None:
-                itemInfo = mikeio.EUMType.Undefined
 
             data.index = utils.make_unique_index(data.index, offset_duplicates=0.001)
 
         super().__init__(data, item, itemInfo, name, quantity)
         self.x = x
         self.y = y
-
-
-if __name__ == "__main__":
-    test = PointModelResult(pd.DataFrame(), 1.0, 2.0, "test", "test", "test")
-
-    assert isinstance(test, protocols.ModelResult)
-    assert isinstance(test, protocols.Comparable)
-    assert isinstance(test, protocols.PointObject)
-    assert isinstance(test, protocols.PointModelResult)
