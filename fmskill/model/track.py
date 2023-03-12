87c240dbb0d2bec6f1b9925740156039f4babc72
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-03-12 22:10:36 +0100

    remove main and unused imports, parse input, WiP

--- fmskill/model/track.py
+++ fmskill/model/track.py
@@ -1,5 +1,6 @@
-from numpy import isin
+from pathlib import Path
 import pandas as pd
+import mikeio
 from fmskill import types, utils
 
 from fmskill.model import protocols
@@ -16,6 +17,16 @@ class TrackModelResult(ModelResultBase):
         quantity: str = None,
         **kwargs
     ) -> None:
+        if isinstance(data, (str, Path)):
+            assert Path(data).suffix == ".dfs0", "File must be a dfs0 file"
+            data = mikeio.read(data, items=item).to_dataframe()
+        elif isinstance(data, mikeio.Dfs0):
+            data = data.read(items=item).to_dataframe()
+        if isinstance(data, mikeio.Dataset):
+            data = data.to_dataframe() if item is None else data[item].to_dataframe()
+        if isinstance(data, mikeio.DataArray):
+            data = data.to_dataframe()
+
         if isinstance(data, pd.DataFrame):
 
             data = utils.rename_coords_pd(data)
@@ -28,10 +39,3 @@ class TrackModelResult(ModelResultBase):
             data.index = utils.make_unique_index(data.index, offset_duplicates=0.001)
 
         super().__init__(data, item, itemInfo, name, quantity, **kwargs)
-
-
-if __name__ == "__main__":
-    test = TrackModelResult(pd.DataFrame(), "test", "test", "test")
-
-    assert isinstance(test, protocols.ModelResult)
-    assert isinstance(test, protocols.Comparable)
