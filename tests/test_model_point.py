645b23fb87205149cb3321f35172b86c95ab1d06
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-03-14 09:58:21 +0100

    new point tests

--- tests/test_model_point.py
+++ tests/test_model_point.py
@@ -1,55 +1,135 @@
+from datetime import datetime
+import numpy as np
 import pandas as pd
 import pytest
 
 import mikeio
-from fmskill import ModelResult, TrackObservation
-from fmskill.model import protocols
+from fmskill import ModelResult, PointObservation
+from fmskill.model import protocols, PointModelResult
 
 
 @pytest.fixture
-def point_df():
-    fn = "tests/testdata/smhi_2095_klagshamn.dfs0"
-    df = mikeio.open(fn).to_dataframe()
-    return df
+def fn_point_noneq():
+    # 1 item with 200 timesteps 2 missing values
+    return "tests/testdata/smhi_2095_klagshamn_200.dfs0"
 
 
 @pytest.fixture
-def track_df():
-    fn = "tests/testdata/altimetry_NorthSea_20171027.csv"
-    return pd.read_csv(fn, index_col=0, parse_dates=True)
+def fn_point_eq():
+    # 1 item with 2017 timesteps many gaps
+    return "tests/testdata/smhi_2095_klagshamn_shifted.dfs0"
 
 
 @pytest.fixture
-def track_from_dfs0():
-    fn = "tests/testdata/NorthSeaHD_extracted_track.dfs0"
-    return mikeio.open(fn).to_dataframe()
+def fn_point_eq2():
+    # 12 items with 2017 timesteps
+    return "tests/testdata/TS.dfs0"
 
 
-# def test_df_modelresultitem(point_df):
-#     df = point_df
-#     df["ones"] = 1.0
-
-# mr1 = DataFramePointModelResultItem(df, item=0)
-# assert isinstance(mr1, ModelResultInterface)
-# assert mr1.start_time == datetime(2015, 1, 1, 1, 0, 0)
-# assert mr1.end_time == datetime(2020, 9, 28, 0, 0, 0)
-# assert mr1.name == "Water Level"
-# assert mr1.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
-
-# # item as string
-# mr2 = DataFramePointModelResultItem(df, item="Water Level")
-# assert len(mr2.data) == len(mr1.data)
-# assert mr2.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
-
-# mr3 = DataFramePointModelResultItem(df[["Water Level"]])
-# assert len(mr3.data) == len(mr1.data)
-
-# Series
-# mr4 = ModelResult(df["Water Level"])
-# assert len(mr4.data) == len(mr1.data)
+@pytest.fixture
+def point_df(fn_point_noneq):
+    return mikeio.open(fn_point_noneq).to_dataframe()
 
 
-def test_df_modelresultitem_itemInfo(point_df):
+def test_point_dfs0(fn_point_eq):
+    fn = fn_point_eq
+
+    ds = mikeio.read(fn)
+    assert len(ds.items) == 1
+    assert ds[0].shape[0] == 2017
+    assert ds[0].start_time == datetime(2019, 4, 8, 0, 0, 0)
+    assert ds[0].end_time == datetime(2019, 4, 15, 0, 0, 0)
+    assert ds[0].dropna().shape[0] == 1875
+
+    mr1 = PointModelResult(fn, item=0)
+    assert mr1.name == "smhi_2095_klagshamn_shifted"
+    assert mr1.item == "Viken: Surface elevation"
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.start_time == datetime(2019, 4, 8, 0, 10, 0)  # first non-NaN
+    assert mr1.end_time == datetime(2019, 4, 14, 23, 35, 0)  # last non-NaN
+    assert len(mr1.data) == 1875
+
+    mr2 = PointModelResult(fn, item="Viken: Surface elevation")
+    assert mr1.data.equals(mr2.data)
+
+    mr3 = PointModelResult(fn)  # default to item=0
+    assert mr1.data.equals(mr3.data)
+
+
+def test_point_dfs0_noneq(fn_point_noneq):
+    fn = fn_point_noneq
+
+    ds = mikeio.read(fn)
+    assert len(ds.items) == 1
+    assert ds.items[0].name == "Water Level"
+    assert len(ds[0].time) == 200
+    mr1 = PointModelResult(fn, item=0, name="test")
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.name == "test"
+    assert mr1.item == "Water Level"
+    assert mr1.start_time == datetime(2015, 1, 1, 1, 0, 0)
+    assert mr1.end_time == datetime(2015, 1, 9, 8, 0, 0)
+    assert len(mr1.data) == 198  # 200 - 2 NaNs
+
+
+def test_point_dfs0_multi_item(fn_point_eq2):
+    fn = fn_point_eq2
+
+    ds = mikeio.read(fn)
+    assert len(ds.items) == 12
+    assert len(ds[0].time) == 2017
+    mr1 = PointModelResult(fn, item=2, name="test")
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.name == "test"
+    assert mr1.item == "Drogden: Surface elevation"
+    assert mr1.start_time == datetime(2018, 3, 4, 0, 0, 0)
+    assert mr1.end_time == datetime(2018, 3, 11, 0, 0, 0)
+    assert len(mr1.data) == 2017
+
+    mr2 = PointModelResult(fn, item="Drogden: Surface elevation")
+    assert isinstance(mr2, PointModelResult)
+    assert mr2.name == "TS"  # default to filename
+    assert mr2.item == "Drogden: Surface elevation"
+    assert np.all(mr2.data.values == mr1.data.values)
+
+    with pytest.raises(ValueError):
+        PointModelResult(fn, name="test")
+
+
+def test_point_dfs0_last_item(fn_point_eq2):
+    fn = fn_point_eq2
+    mr1 = PointModelResult(fn, item=-1, name="test")
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.name == "test"
+    assert mr1.item == "Klagshamn: V velocity"
+
+
+def test_point_df_item(point_df):
+    df = point_df
+    df["ones"] = 1.0
+
+    mr1 = PointModelResult(df, item=0)
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.start_time == datetime(2015, 1, 1, 1, 0, 0)
+    assert mr1.end_time == datetime(2015, 1, 9, 8, 0, 0)
+    assert mr1.name == "Water Level"
+    assert mr1.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
+
+    # item as string
+    mr2 = PointModelResult(df, item="Water Level")
+    assert len(mr2.data) == len(mr1.data)
+    assert mr2.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
+
+    mr3 = PointModelResult(df[["Water Level"]])
+    assert len(mr3.data) == len(mr1.data)
+
+    # Series
+    mr4 = PointModelResult(df["Water Level"])
+    assert len(mr4.data) == len(mr1.data)
+    assert np.all(mr4.data.values == mr1.data.values)
+
+
+def test_point_df_itemInfo(point_df):
     df = point_df
     df["ones"] = 1.0
     itemInfo = mikeio.EUMType.Surface_Elevation
@@ -61,118 +141,22 @@ def test_df_modelresultitem_itemInfo(point_df):
     assert mr2.itemInfo == mikeio.ItemInfo("WL", mikeio.EUMType.Surface_Elevation)
 
 
-# def test_df_modelresult(point_df):
-#     df = point_df
-#     df["ones"] = 1.0
+def test_point_df(point_df):
+    df = point_df
+    df["ones"] = 1.0
 
-#     mr1 = DataFramePointModelResultItem(df, item=0)
-#     assert isinstance(mr1, ModelResultInterface)
-#     assert mr1.start_time == datetime(2015, 1, 1, 1, 0, 0)
-#     assert mr1.end_time == datetime(2020, 9, 28, 0, 0, 0)
-#     assert mr1.name == "Water Level"
+    mr1 = PointModelResult(df, item=0)
+    assert isinstance(mr1, PointModelResult)
+    assert mr1.start_time == datetime(2015, 1, 1, 1, 0, 0)
+    assert mr1.end_time == datetime(2015, 1, 9, 8, 0, 0)
+    assert mr1.name == "Water Level"
 
 
-# def test_point_df_model_extract(point_df):
+# TODO
+# def test_point_df_compare(point_df):
 #     df = point_df
 #     mr1 = DataFramePointModelResultItem(df, item=0)
 #     o1 = PointObservation(df, item=0)
-#     c = mr1.extract_observation(o1)
+#     c = mr1.compare(o1)
 #     assert c.score() == 0.0  # o1=mr1
 #     assert c.n_points == len(o1.data.dropna())
-
-
-def test_track_df_modelresultitem(track_df):
-    df = track_df
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr1 = ModelResult(df, item=2, geometry_type="track")
-    assert isinstance(mr1, protocols.ModelResult)
-    assert "Item: surface_elevation" in repr(mr1)
-
-    # item as string
-    mr2 = ModelResult(df, item="surface_elevation")
-    assert len(mr2.data) == len(mr1.data)
-    assert mr2.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
-
-    # mr3 = DataFramePointModelResultItem(df[["surface_elevation"]])
-    # assert len(mr3.data) == len(mr1.data)
-    # assert mr3.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Undefined)
-
-
-def test_track_df_modelresultitem_iteminfo(track_df):
-    df = track_df
-    itemInfo = mikeio.EUMType.Surface_Elevation
-    mr1 = ModelResult(df, item="surface_elevation", itemInfo=itemInfo)
-    assert mr1.itemInfo == mikeio.ItemInfo(mikeio.EUMType.Surface_Elevation)
-
-    itemInfo = mikeio.ItemInfo("WL", mikeio.EUMType.Surface_Elevation)
-    mr2 = ModelResult(df, item="surface_elevation", itemInfo=itemInfo)
-    assert mr2.itemInfo == mikeio.ItemInfo("WL", mikeio.EUMType.Surface_Elevation)
-
-
-def test_track_df_modelresult(track_df):
-    df = track_df
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr1 = ModelResult(df, item=2, geometry_type="track")
-    assert isinstance(mr1, protocols.ModelResult)
-    assert len(mr1.data.columns) == 3
-    assert mr1.item == "surface_elevation"
-
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr3 = ModelResult(df, item=2, geometry_type="track")
-    assert len(mr3.data) == len(mr1.data)
-    assert isinstance(mr3, protocols.ModelResult)
-
-
-def test_track_from_dfs0_df_modelresult(track_from_dfs0):
-    df = track_from_dfs0
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr1 = ModelResult(df, item=-1, geometry_type="track")
-    assert isinstance(mr1, protocols.ModelResult)
-    assert len(mr1.data.columns) == 3
-
-
-def test_track_df_tweak_modelresult(track_df):
-    df = track_df
-    # Reorder columns
-    df = df[
-        [
-            "surface_elevation",
-            "lon",
-            "lat",
-        ]
-    ]
-
-    # Which columns are used for position, lon and lat?
-    with pytest.raises(ValueError):
-        ModelResult(df, geometry_type="track")
-
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr1 = ModelResult(df, geometry_type="track", x_item=1, y_item="lat")
-    assert isinstance(mr1, protocols.ModelResult)
-    assert mr1.item_name == "surface_elevation"
-
-    # Rename
-    df = df.copy()
-    df.columns = ["wl", "longitude", "latitude"]
-    df["ones"] = 1.0  # add extra column
-
-    with pytest.raises(ValueError):
-        ModelResult(df, geometry_type="track")
-
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr3 = ModelResult(
-            df, item="wl", geometry_type="track", x_item="longitude", y_item="latitude"
-        )
-    assert isinstance(mr3, protocols.ModelResult)
-
-
-def test_track_df_model_extract(track_df):
-    df = track_df
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        mr1 = ModelResult(df, item=2)
-    with pytest.warns(UserWarning, match="Time axis has duplicate entries"):
-        o1 = TrackObservation(df, item=2)
-    c = mr1.extract_observation(o1)
-    assert c.score() == 0.0  # o1=mr1
-    assert len(o1.data.dropna()) == 1110
-    assert c.n_points == 1110
