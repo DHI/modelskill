504059df1e4f22e7026ee13ad70fb572e7abbf9f
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-03-14 16:47:57 +0100

    validate in domain and in time

--- fmskill/model/extraction.py
+++ fmskill/model/extraction.py
@@ -1,10 +1,40 @@
 import mikeio
 import numpy as np
+import pandas as pd
 import xarray as xr
 
 from fmskill import utils
 from fmskill.model import PointModelResult, TrackModelResult, protocols
 from fmskill.observation import PointObservation, TrackObservation
+
+
+def _xy_in_xr_domain(data: xr.Dataset, x: float, y: float) -> bool:
+    if (x is None) or (y is None):
+        raise ValueError(f"Cannot determine if point ({x}, {y}) is inside domain!")
+    xmin = data.x.values.min()
+    xmax = data.x.values.max()
+    ymin = data.y.values.min()
+    ymax = data.y.values.max()
+    return (x >= xmin) & (x <= xmax) & (y >= ymin) & (y <= ymax)
+
+
+def _any_obs_in_model_time(
+    time_obs: pd.DatetimeIndex, time_model: pd.DatetimeIndex
+) -> bool:
+    """Check if any observation times are in model time range"""
+    return (time_obs[-1] >= time_model[0]) & (time_obs[0] <= time_model[-1])
+
+
+def _validate_any_obs_in_model_time(
+    obs_name: str, time_obs: pd.DatetimeIndex, time_model: pd.DatetimeIndex
+) -> None:
+    """Check if any observation times are in model time range"""
+    ok = _any_obs_in_model_time(time_obs, time_model)
+    if not ok:
+        raise ValueError(
+            f"Observation '{obs_name}' outside model time range! "
+            + f"({time_obs[0]} - {time_obs[-1]}) not in ({time_model[0]} - {time_model[-1]})"
+        )
 
 
 def point_obs_from_xr_mr(
@@ -20,6 +50,13 @@ def point_obs_from_xr_mr(
             + f"because it has None position x={x}, y={y}. Please provide position "
             + "when creating PointObservation."
         )
+    if not _xy_in_xr_domain(mr.data, x, y):
+        raise ValueError(
+            f"PointObservation '{observation.name}' ({x}, {y}) is outside model domain!"
+        )
+
+    _validate_any_obs_in_model_time(observation.name, observation.data.index, mr.time)
+
     da = mr.data[mr.item].interp(coords=dict(x=x, y=y), method="nearest")
     df = da.to_dataframe().drop(columns=["x", "y"])
     df = df.rename(columns={df.columns[-1]: mr.name})
@@ -40,6 +77,8 @@ def extract_track_from_xr(
 ) -> TrackModelResult:
     """Extract a TrackModelResult from a GridModelResult (when data is a xarray.Dataset),
     given a TrackObservation."""
+
+    _validate_any_obs_in_model_time(observation.name, observation.data.index, mr.time)
 
     renamed_obs_data = utils.rename_coords_pd(observation.data)
     t = xr.DataArray(renamed_obs_data.index, dims="track")
@@ -67,11 +106,16 @@ def extract_point_from_dfsu(
 
     assert isinstance(mr.data, (mikeio.dfsu.Dfsu2DH, mikeio.DataArray, mikeio.Dataset))
 
-    if not mr.data.geometry.contains([observation.x, observation.y]):
-        raise ValueError("Observation outside domain")
+    x, y = observation.x, observation.y
+    if not mr.data.geometry.contains([x, y]):
+        raise ValueError(
+            f"PointObservation '{observation.name}' ({x}, {y}) outside model domain!"
+        )
+
+    _validate_any_obs_in_model_time(observation.name, observation.data.index, mr.time)
 
     # TODO: interp2d
-    xy = np.atleast_2d([observation.x, observation.y])
+    xy = np.atleast_2d([x, y])
     elemids = mr.data.geometry.find_index(coords=xy)
     if isinstance(mr.data, mikeio.dfsu.Dfsu2DH):
         ds_model = mr.data.read(elements=elemids, items=[mr.item])
@@ -81,9 +125,6 @@ def extract_point_from_dfsu(
         da = mr.data.isel(element=elemids)
         ds_model = mikeio.Dataset({da.name: da})
     ds_model.rename({ds_model.items[0].name: mr.name}, inplace=True)
-
-    # df = ds_model.to_dataframe().dropna()
-    # df.index = utils.make_unique_index(df.index, offset_duplicates=0.001)
 
     return PointModelResult(
         data=ds_model,
@@ -104,34 +145,18 @@ def extract_track_from_dfsu(
 
     assert isinstance(mr.data, (mikeio.dfsu.Dfsu2DH, mikeio.DataArray, mikeio.Dataset))
 
+    _validate_any_obs_in_model_time(observation.name, observation.data.index, mr.time)
+
     if isinstance(mr.data, mikeio.dfsu.Dfsu2DH):
         ds_model = mr.data.extract_track(track=observation.data, items=[mr.item])
     elif isinstance(mr.data, (mikeio.Dataset, mikeio.DataArray)):
-        ds_model = mr.data.extract_track(track=observation.data)    
+        ds_model = mr.data.extract_track(track=observation.data)
     ds_model.rename({ds_model.items[-1].name: mr.name}, inplace=True)
 
     return TrackModelResult(
         data=ds_model.dropna(),  # .to_dataframe().dropna(),
         item=mr.name,
         itemInfo=mr.itemInfo,
-        name=mr.name,
-        quantity=mr.quantity,
-    )
-
-
-def extract_point_from_mike_ds(
-    mr: protocols.Extractable, observation: PointObservation
-) -> PointModelResult:
-
-    dap = mr.data.sel(x=observation.x, y=observation.y)
-    dap.name = mr.name
-    # ds_model.rename({ds_model.items[0].name: self.name}, inplace=True)
-
-    return PointModelResult(
-        data=mikeio.Dataset(dap).to_dataframe().dropna(),
-        x=dap.x,
-        y=dap.y,
-        item=mr.item,
         name=mr.name,
         quantity=mr.quantity,
     )
