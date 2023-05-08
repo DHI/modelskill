42863f031aa3f3219a806eb1f0228144d72e68e9
Henrik Andersson <jan@dhigroup.com>
2023-05-08 13:25:51 +0200

    Types

--- fmskill/timeseries.py
+++ fmskill/timeseries.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from dataclasses import dataclass
-from typing import Callable, ClassVar, Protocol
+from typing import ClassVar, Protocol
 
 import pandas as pd
-
 
 from .types import Quantity
 
@@ -19,6 +18,9 @@ class TimeSeriesPlotter(Protocol):
         pass
 
     def hist(self):
+        pass
+
+    def timeseries(self):
         pass
 
 
@@ -109,21 +111,26 @@ class MatplotlibTimeSeriesPlotter(TimeSeriesPlotter):
 
 class PlotlyTimeSeriesPlotter(TimeSeriesPlotter):
     def __init__(self, ts: "TimeSeries") -> None:
-        self._ts: pd.DataFrame = ts
+        self._ts: "TimeSeries" = ts
 
     def __call__(self):
         self.plot()
 
-    def plot(self):
-        import plotly.express as px
+    def timeseries(self):
+        self.plot()
 
-        fig = px.line(self._ts.data)
+    def plot(self):
+        import plotly.express as px  # type: ignore
+
+        fig = px.line(self._ts.data, color_discrete_sequence=[self._ts.color])
         fig.show()
 
-    def hist(self):
-        import plotly.express as px
+    def hist(self, bins=100, **kwargs):
+        import plotly.express as px  # type: ignore
 
-        fig = px.histogram(self._ts.data)
+        fig = px.histogram(
+            self._ts.data, nbins=bins, color_discrete_sequence=[self._ts.color]
+        )
         fig.show()
 
 
@@ -131,35 +138,38 @@ class PlotlyTimeSeriesPlotter(TimeSeriesPlotter):
 class TimeSeries:
     """Time series data"""
 
-    plotter: ClassVar = (
-        MatplotlibTimeSeriesPlotter  # TODO is this the best option to choose a plotter?
-    )
+    plotter: ClassVar = MatplotlibTimeSeriesPlotter  # TODO is this the best option to choose a plotter? Can we use the settings module?
 
     name: str
     data: pd.DataFrame
     quantity: Quantity
     color: str = "#d62728"
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self.quantity is None:
             self.quantity = Quantity.undefined()
+
+        assert isinstance(self.data, pd.DataFrame), "data must be a pandas.DataFrame"
+        assert isinstance(
+            self.data.index, pd.DatetimeIndex
+        ), "index must be a DatetimeIndex"
 
         self.plot: TimeSeriesPlotter = TimeSeries.plotter(self)
         self.hist = self.plot.hist  # TODO remove this
 
     @property
     def time(self) -> pd.DatetimeIndex:
-        return self.data.index
+        return self.data.index  # type: ignore
 
     @property
     def start_time(self) -> datetime:
-        return self.time[0]
+        return self.time[0]  # type: ignore
 
     @property
     def end_time(self) -> datetime:
-        return self.time[-1]
+        return self.time[-1]  # type: ignore
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__}> '{self.name}'"
 
     def trim(
@@ -179,4 +189,4 @@ class TimeSeries:
         # Expand time interval with buffer
         start_time = pd.Timestamp(start_time) - pd.Timedelta(buffer)
         end_time = pd.Timestamp(end_time) + pd.Timedelta(buffer)
-        self.data = self.data.loc[start_time:end_time]
+        self.data = self.data.loc[start_time:end_time]  # type: ignore
