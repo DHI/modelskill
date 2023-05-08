497813c62cdb6b61bfa7d8bd9b41f68bddf23d36
Henrik Andersson <jan@dhigroup.com>
2023-05-08 12:01:36 +0200

    Plotter classes

--- fmskill/timeseries.py
+++ fmskill/timeseries.py
@@ -1,14 +1,118 @@
 from datetime import datetime
 from dataclasses import dataclass
+from typing import Callable, ClassVar
 
 import pandas as pd
 
 from .types import Quantity
 
 
+class MatplotlibTimeSeriesPlotter(Callable):
+    def __init__(self, ts: "TimeSeries") -> None:
+        self._ts = ts
+
+    def __call__(self, title=None, color=None, marker=".", linestyle="None", **kwargs):
+        self.plot(
+            title=title, color=color, marker=marker, linestyle=linestyle, **kwargs
+        )
+
+    def timeseries(
+        self, title=None, color=None, marker=".", linestyle="None", **kwargs
+    ):
+        """plot timeseries
+
+        Wraps pandas.DataFrame plot() method.
+
+        Parameters
+        ----------
+        title : str, optional
+            plot title, default: [name]
+        color : str, optional
+            plot color, by default '#d62728'
+        marker : str, optional
+            plot marker, by default '.'
+        linestyle : str, optional
+            line style, by default None
+        kwargs: other keyword arguments to df.plot()
+        """
+        self.plot()
+
+    def plot(self, title=None, color=None, marker=".", linestyle="None", **kwargs):
+        """plot timeseries
+
+        Wraps pandas.DataFrame plot() method.
+
+        Parameters
+        ----------
+        title : str, optional
+            plot title, default: [name]
+        color : str, optional
+            plot color, by default '#d62728'
+        marker : str, optional
+            plot marker, by default '.'
+        linestyle : str, optional
+            line style, by default None
+        kwargs: other keyword arguments to df.plot()
+        """
+        kwargs["color"] = self._ts.color if color is None else color
+        ax = self._ts.data.plot(marker=marker, linestyle=linestyle, **kwargs)
+
+        title = self._ts.name if title is None else title
+        ax.set_title(title)
+
+        ax.set_ylabel(str(self._ts.quantity))
+        return ax
+
+    def hist(self, bins=100, title=None, color=None, **kwargs):
+        """plot histogram of timeseries values
+
+        Wraps pandas.DataFrame hist() method.
+
+        Parameters
+        ----------
+        bins : int, optional
+            specification of bins, by default 100
+        title : str, optional
+            plot title, default: observation name
+        color : str, optional
+            plot color, by default "#d62728"
+        kwargs : other keyword arguments to df.hist()
+
+        Returns
+        -------
+        matplotlib axes
+        """
+        title = self._ts.name if title is None else title
+
+        kwargs["color"] = self._ts.color if color is None else color
+
+        ax = self._ts.data.iloc[:, -1].hist(bins=bins, **kwargs)
+        ax.set_title(title)
+        ax.set_xlabel(str(self._ts.quantity))
+        return ax
+
+
+class PlotlyTimeSeriesPlotter(Callable):
+    def __init__(self, ts: "TimeSeries") -> None:
+        self._ts: pd.DataFrame = ts
+
+    def __call__(self):
+        self.plot()
+
+    def plot(self):
+        import plotly.express as px
+
+        fig = px.line(self._ts.data)
+        fig.show()
+
+
 @dataclass
 class TimeSeries:
     """Time series data"""
+
+    plotter: ClassVar = (
+        MatplotlibTimeSeriesPlotter  # TODO is this the best option to choose a plotter?
+    )
 
     name: str
     data: pd.DataFrame
@@ -18,6 +122,8 @@ class TimeSeries:
     def __post_init__(self):
         if self.quantity is None:
             self.quantity = Quantity.undefined()
+
+        self.plot = TimeSeries.plotter(self)
 
     @property
     def time(self) -> pd.DatetimeIndex:
@@ -52,59 +158,3 @@ class TimeSeries:
         start_time = pd.Timestamp(start_time) - pd.Timedelta(buffer)
         end_time = pd.Timestamp(end_time) + pd.Timedelta(buffer)
         self.data = self.data.loc[start_time:end_time]
-
-    # TODO move plot method defined in a separate class, .e.g MatplotlibTimeSeriesPlotter, PlotlyTimeSeriesPlotter
-    def plot(self, title=None, color=None, marker=".", linestyle="None", **kwargs):
-        """plot observation timeseries
-
-        Wraps pandas.DataFrame plot() method.
-
-        Parameters
-        ----------
-        title : str, optional
-            plot title, default: [name]
-        color : str, optional
-            plot color, by default '#d62728'
-        marker : str, optional
-            plot marker, by default '.'
-        linestyle : str, optional
-            line style, by default None
-        kwargs: other keyword arguments to df.plot()
-        """
-        kwargs["color"] = self.color if color is None else color
-        ax = self.data.plot(marker=marker, linestyle=linestyle, **kwargs)
-
-        title = self.name if title is None else title
-        ax.set_title(title)
-
-        ax.set_ylabel(str(self.quantity))
-        return ax
-
-    def hist(self, bins=100, title=None, color=None, **kwargs):
-        """plot histogram of timeseries values
-
-        Wraps pandas.DataFrame hist() method.
-
-        Parameters
-        ----------
-        bins : int, optional
-            specification of bins, by default 100
-        title : str, optional
-            plot title, default: observation name
-        color : str, optional
-            plot color, by default "#d62728"
-        kwargs : other keyword arguments to df.hist()
-
-        Returns
-        -------
-        matplotlib axes
-        """
-        title = self.name if title is None else title
-
-        # TODO color
-        kwargs["color"] = self.color if color is None else color
-
-        ax = self.data.iloc[:, -1].hist(bins=bins, **kwargs)
-        ax.set_title(title)
-        ax.set_xlabel(str(self.quantity))
-        return ax
