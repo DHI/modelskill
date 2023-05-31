ab1cfb9242d10e5e00e4d1b45d8158efcfc6bf57
Henrik Andersson <jan@dhigroup.com>
2023-05-31 11:44:21 +0200

    Docs logo

--- docs/conf.py
+++ docs/conf.py
@@ -54,4 +54,4 @@ html_theme = "sphinx_book_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-html_logo = "../images/logo/mike-fm-skill-rgb.svg"
+html_logo = "../images/logo/modelskill.svg"
