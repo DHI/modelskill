cc60c3fdcaeddf11b2098028c62b9ac4072e5633
Henrik Andersson <jan@dhigroup.com>
2023-07-14 16:11:24 +0200

    Refactored

--- modelskill/rose.py
+++ modelskill/rose.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Tuple
+from typing import Optional, List, Tuple, Union
 
 import numpy as np
 import matplotlib as mpl
@@ -19,12 +19,10 @@ def wind_rose(
     calm_text="Calm",
     r_step=0.1,
     r_max=None,
-    cbar_label=None,
     legend=True,
     cmap1="viridis",
     cmap2="Greys",
     mag_bins: Optional[List[float]] = None,
-    invert_dir=False,
     max_bin=None,  # TODO rename to vmax?
     **kwargs,
 ):
@@ -32,7 +30,7 @@ def wind_rose(
     """Function for plotting Wave, Wind or Current roses with Calms.
     Parameters:
         data: array-like
-            array with 2 or 4 columns
+            array with 4 columns
         mag_step: float, (optional) Default= None
             discretization for magnitude (delta_r, in radial direction )
         ax: Matplotlib axis Default= None
@@ -53,35 +51,24 @@ def wind_rose(
             max value to truncate the data, eg,  max_bin=1.0 if hm0=1m is the desired final bin.
         mag_bins : array of floats (optional) Default = None
             force bins to array of values, e.g. when specifying non-equidistant bins.
-        cbar_label: string. Default= None
-            Label of the legend, if None, mag it is used. Examples cbar_label='Hm0', cbar_label='WS' or cbar_label='CS'
         legend: boolean. Default= True
             if None the legend is not ploted
         cmap1 : string. Default= 'viridis'
             colormap for main axis
         cmap2 : string. Default= 'Greys'
             colormap for secondary axis
-        invert_dir: string (optional)
-            invert directions (coming-from, going to). Default= False
 
     ------------------------------------------------------------------------------------------------
     Returns
         ax: Matplotlib axes
     """
-    cols = data.columns.values
-    data_1 = data.iloc[:, 0:2].copy()
-    if data.shape[1] == 4:
-        second_rose = True
-        data_2 = data.iloc[:, 2:4].copy()
-        mag1, dir1, mag2, dir2 = cols
-    else:
-        second_rose = False
-        mag1, dir1 = cols
+    data_1 = data[:, 0:2]
+    data_2 = data[:, 2:4]
 
     # magnitude bins
     ui, vmin, vmax = pretty_intervals(
-        data_1[mag1],
-        data_2[mag2],
+        data_1[:,0],
+        data_2[:,0],
         mag_bins,
         mag_step,
         calm_threshold,
@@ -96,67 +83,26 @@ def wind_rose(
     )
     thetac = thetai[:-1] + dir_step / 2
 
+    mask_1 = data_1[:,0] >= vmin
+    mask_2 = data_2[:,0] >= vmin
+
     ### compute total calms
     N = len(data_1)
-    calm = len(data_1[data_1[mag1] < vmin]) / N
-    calm_value = calm
+    calm = len(data_1[~mask_1]) / N
+    calm2 = len(data_2[~mask_2]) / N
 
-    calm_2 = len(data_2[data_2[mag2] < vmin]) / N
-    calm_value_2 = calm_2
-
-    ### add 360 to all dir1s from 0 to dir_step/2
-    if invert_dir:
-        d = [np.mod(x - 180, 360) for x in data_1[dir1].values]
-        d2 = [np.mod(x - 180, 360) for x in data_2[dir2].values]
-    else:
-        d = data_1[dir1]  # dir1 as coming from
-        d2 = data_2[dir2]
-
-    data_1["dir_proxy"] = d
-    data_1.loc[data_1[dir1] < dir_step / 2, "dir_proxy"] = (
-        data_1.loc[data_1[dir1] < dir_step / 2, dir1] + 360
-    )
-
-    data_2["dir_proxy"] = d2
-    data_2.loc[data_2[dir2] < dir_step / 2, "dir_proxy"] = (
-        data_2.loc[data_2[dir2] < dir_step / 2, dir2] + 360
-    )
-
-    ### compute histograms
-    counts, _, _ = np.histogram2d(
-        data_1[data_1[mag1] >= vmin][mag1],
-        data_1[data_1[mag1] >= vmin]["dir_proxy"],
-        bins=[ui, thetai],
-    )
-    counts = counts / N
-
-    counts_2, _, _ = np.histogram2d(
-        data_2[data_2[mag2] >= vmin][mag2],
-        data_2[data_2[mag2] >= vmin]["dir_proxy"],
-        bins=[ui, thetai],
-    )
-    counts_2 = counts_2 / N
+    counts, counts_2 = _calc_histograms(data_1=data_1, mask_1=mask_1, data_2=data_2, mask_2=mask_2, ui=ui, thetai=thetai)
 
     ### compute radial ticks
-    if r_max is None:
-        rmax = np.ceil((counts.sum(axis=0).max() + r_step) / r_step) * r_step
-    else:
-        rmax = r_max
-
-    ri = np.linspace(0, rmax, int(rmax / r_step) + 1)
-    ri = ri[1:-1]
+    ri, rmax = _calc_radial_ticks(counts=counts, step=r_step, stop=r_max)
 
     # Resize calm
     calm = resize_calm
-    if isinstance(cmap1, str):
-        cmap = mpl.cm.get_cmap(cmap1)
-    elif isinstance(cmap1, mpl.colors.ListedColormap):
-        cmap = cmap1
-    else:
-        raise ValueError(f"Invalid cmap {cmap1}")
+
+    cmap = _get_cmap(cmap1)
     norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
     colors_ = [cmap(norm(x)) for x in ui]
-    ### setup figure
+    
     if ax is None:
         _, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(projection="polar"))
 
@@ -172,18 +118,7 @@ def wind_rose(
     ax.set_rlabel_position(5)
     ### add calms
     if vmin > 0:
-        ax.bar(np.pi, calm, color="white", ec="k", zorder=0)
-        ax.bar(
-            np.pi, calm, width=2 * np.pi, label="_nolegend_", color="white", zorder=3
-        )
-        ax.text(
-            0.5,
-            0.5,
-            calm_text,
-            horizontalalignment="center",
-            verticalalignment="center",
-            transform=ax.transAxes,
-        )
+        _add_calms_to_ax(ax, threshold=calm, text=calm_text)
 
     ### plot each bar of the histogram
     patches = []
@@ -217,131 +152,55 @@ def wind_rose(
     )
     ax.add_collection(p)
 
-    percentages = np.sum(counts, axis=1) * 100
+    if legend:
+        _add_legend_to_ax(ax, colors=colors_, vmin=vmin, ui=ui, calm=calm, counts=counts, label="Model", primary=True)
+        
+    cmap = _get_cmap(cmap2)
+    norm = mpl.colors.Normalize(vmin=0, vmax=vmax)
+    colors_ = [cmap(norm(x)) for x in ui]
+    
+    ### plot each bar of the histogram
+    patches = []
+    colors = []
+    cumcount = counts_2.cumsum(axis=0)
 
-    # Labels
-    if cbar_label is not None:
-        label = cbar_label
-    else:
-        label = mag1
+    dir_step2 = dir_step / 2
 
-    legend_items = []
+    arc_x = np.deg2rad(
+        np.linspace(thetac - dir_step2 / 2, thetac + dir_step2 / 2, arc_res)
+    )
+    # Loop through velocities
+    for i, vel in enumerate(ui[1:]):
+        # Loop through directions
+        for j, c in enumerate(counts[i]):
+            arc_xj = np.concatenate([arc_x[:, j], np.flip(arc_x[:, j])])
+            arc_yj = np.concatenate(
+                [np.full(arc_res, calm), np.full(arc_res, calm + cumcount[i, j])]
+            )
 
-    for j in range(len(ui[1:-1])):
-        legend_items.append(f"{np.round(ui[j],2)} - {np.round(ui[j+1],2)}")
-    _items = [f"<{vmin} ({np.round(calm_value*100,2)}%)"]
-    _items.extend(legend_items)
-    _items.append(f">= {ui[-2]} ({np.round( percentages[-1],2)}%)")
+            polygon = Polygon(
+                np.array((arc_xj, arc_yj)).T, True
+            )  # Conflict with shapely
+            patches.append(polygon)
+            colors.append(cmap(norm(vel)))
+
+    p = PatchCollection(
+        np.flip(patches),
+        facecolors=np.flip(colors, axis=0),
+        edgecolor="k",
+        linewidth=0.5,
+    )
+    ax.add_collection(p)
 
     if legend:
-        handles = [Rectangle((0, 0), 1, 1, color=c, ec="k") for c in colors_[:]]
-        handles[0].set_color("white")
-        handles[0].set_ec("k")
-
-        if "right_label" in kwargs:
-            right_label = kwargs["right_label"]
-            rlabel = f"{right_label}\n" + label
-        else:
-            rlabel = label
-
-        leg = Legend(
-            ax,
-            handles[::-1],
-            _items[::-1],
-            frameon=True,
-            title=rlabel,
-            bbox_to_anchor=(1.05, -0.06, 0.1, 0.8),
-            loc="lower left",
-        )
-        box_width = 0.32
-        ax_right = ax.inset_axes([1.15, -0.05, box_width * 1.15, 0.5])
-        ax_right.axis("off")
-        ax.add_artist(leg)
-
-    if second_rose:
-        if isinstance(cmap2, str):
-            cmap = mpl.cm.get_cmap(cmap2)
-        elif isinstance(cmap2, mpl.colors.ListedColormap):
-            cmap = cmap2
-        else:
-            raise ValueError(f"Invalid cmap {cmap2}")
-        norm = mpl.colors.Normalize(vmin=0, vmax=vmax)
-        percentages = np.sum(counts_2, axis=1) * 100
-        colors_ = [cmap(norm(x)) for x in ui]
-        if "left_label" in kwargs:
-            left_label = kwargs["left_label"]
-            llabel = f"{left_label}\n" + label
-        else:
-            llabel = label
-
-        ### plot each bar of the histogram
-        patches = []
-        colors = []
-        cumcount = counts_2.cumsum(axis=0)
-
-        dir_step2 = dir_step / 2
-
-        arc_x = np.deg2rad(
-            np.linspace(thetac - dir_step2 / 2, thetac + dir_step2 / 2, arc_res)
-        )
-        # Loop through velocities
-        for i, vel in enumerate(ui[1:]):
-            # Loop through directions
-            for j, c in enumerate(counts[i]):
-                arc_xj = np.concatenate([arc_x[:, j], np.flip(arc_x[:, j])])
-                arc_yj = np.concatenate(
-                    [np.full(arc_res, calm), np.full(arc_res, calm + cumcount[i, j])]
-                )
-
-                polygon = Polygon(
-                    np.array((arc_xj, arc_yj)).T, True
-                )  # Conflict with shapely
-                patches.append(polygon)
-                colors.append(cmap(norm(vel)))
-
-        p = PatchCollection(
-            np.flip(patches),
-            facecolors=np.flip(colors, axis=0),
-            edgecolor="k",
-            linewidth=0.5,
-        )
-        ax.add_collection(p)
-
-        _items[0] = f"<{vmin} ({np.round(calm_value_2*100,2)}%)"
-        _items[-1] = f">= {ui[-2]} ({np.round(percentages[-1],2)}%)"
-
-        handles = [Rectangle((0, 0), 1, 1, color=c, ec="k") for c in colors_[:]]
-        handles[0].set_color("white")
-        handles[0].set_ec("k")
-
-        leg = Legend(
-            ax,
-            handles[::-1],
-            _items[::-1],
-            frameon=True,
-            title=llabel,
-            bbox_to_anchor=(-0.13, -0.06, 0.1, 0.8),
-            loc="lower right",
-        )
-
-        box_width = 0.32
-        ax_left = ax.inset_axes([-box_width * 1.15, -0.05, box_width * 1.15, 0.5])
-        ax_left.axis("off")
-        ax.add_artist(leg)
+        _add_legend_to_ax(ax, colors=colors_, vmin=vmin, ui=ui, calm=calm2, counts=counts_2, label="Observation", primary=False)
 
     if "watermark" in kwargs:
-        watermark = kwargs["watermark"]
-        text = AnchoredText(
-            watermark,
-            "center right",
-            frameon=False,
-            borderpad=-27.5,
-            prop=dict(fontsize="xx-small", alpha=0.15, rotation=90),
-        )
-        ax.add_artist(text)
+        _add_watermark_to_ax(ax, kwargs["watermark"])
     return ax
 
 
+# TODO not sure this belongs in the rose module
 def pretty_intervals(
     data_1: np.typing.ArrayLike,
     data_2: np.typing.ArrayLike,
@@ -362,11 +221,14 @@ def pretty_intervals(
     if mag_bins is not None:
 
         # Set values
+
+        assert len(mag_bins) >= 3, "Must have at least 3 bins"
         mag_bins_ = np.array(mag_bins)
         ui = np.concatenate((mag_bins_, mag_bins_[[-1]] * 999))
         vmin = ui[0]
         max_bin = ui[-2]
         dbin = np.diff(ui)[-2]
+        vmax = max_bin + dbin * 2  # TODO what is happening here?
 
     else:
         if mag_step is None:
@@ -402,5 +264,113 @@ def pretty_intervals(
         # Round bins to 3 decimal, this could be an input
         ui = [np.round(x, 3) for x in ui]
 
-        # TODO return a better object
-        return ui, vmin, vmax
+    # TODO return a better object
+    return ui, vmin, vmax
+
+def _calc_histograms(*,data_1, mask_1, data_2, mask_2, ui, thetai) -> Tuple[np.ndarray, np.ndarray]:
+
+    N = len(data_1)
+    counts, _, _ = np.histogram2d(
+        data_1[mask_1][:,0],
+        data_1[mask_1][:,1],
+        bins=[ui, thetai],
+    )
+    counts = counts / N
+
+    counts_2, _, _ = np.histogram2d(
+        data_2[mask_2][:,0],
+        data_2[mask_2][:,1],
+        bins=[ui, thetai],
+    )
+    counts_2 = counts_2 / N
+
+    return counts, counts_2
+
+def _calc_radial_ticks(*, counts: np.ndarray, step: float, stop: Optional[float]) -> np.ndarray:
+    cmax = counts.sum(axis=0).max()
+    if stop is None:
+        rmax = np.ceil((cmax + step) / step) * step
+    else:
+        rmax = stop
+
+    ri = np.linspace(0, rmax, int(rmax / step) + 1)
+    ri = ri[1:-1]
+
+    return ri, rmax
+
+def _add_calms_to_ax(ax,*, threshold: np.ndarray, text: str) -> None:
+    ax.bar(np.pi, threshold, color="white", ec="k", zorder=0)
+    ax.bar(
+        np.pi, threshold, width=2 * np.pi, label="_nolegend_", color="white", zorder=3
+    )
+    ax.text(
+        0.5,
+        0.5,
+        text,
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+    )
+
+def _add_legend_to_ax(ax,*,colors, vmin, ui, calm, counts, label, primary: bool) -> None:
+
+    percentages = np.sum(counts, axis=1) * 100
+
+    legend_items = []
+
+    for j in range(len(ui[1:-1])):
+        legend_items.append(f"{np.round(ui[j],2)} - {np.round(ui[j+1],2)}")
+    items = [f"<{vmin} ({np.round(calm*100,2)}%)"]
+    items.extend(legend_items)
+    items.append(f">= {ui[-2]} ({np.round( percentages[-1],2)}%)")
+
+    handles = [Rectangle((0, 0), 1, 1, color=c, ec="k") for c in colors]
+    handles[0].set_color("white")
+    handles[0].set_ec("k")
+
+    if primary:
+        bbox_to_anchor = (1.05, -0.06, 0.1, 0.8)
+        loc = "lower left"
+    else:
+        bbox_to_anchor = (-0.13, -0.06, 0.1, 0.8)
+        loc="lower right"
+        items[0] = f"<{vmin} ({np.round(calm*100,2)}%)"
+        items[-1] = f">= {ui[-2]} ({np.round(percentages[-1],2)}%)"
+        
+    leg = Legend(
+        ax,
+        handles[::-1],
+        items[::-1],
+        frameon=True,
+        title=label,
+        bbox_to_anchor=bbox_to_anchor,
+        loc=loc,
+    )
+    box_width = 0.32
+
+    if primary:
+        ax_left = ax.inset_axes([-box_width * 1.15, -0.05, box_width * 1.15, 0.5])
+        ax_left.axis("off")
+    else:
+        ax_right = ax.inset_axes([1.15, -0.05, box_width * 1.15, 0.5])
+        ax_right.axis("off")
+    ax.add_artist(leg)
+
+def _add_watermark_to_ax(ax, watermark: str) -> None:
+    text = AnchoredText(
+            watermark,
+            "center right",
+            frameon=False,
+            borderpad=-27.5,
+            prop=dict(fontsize="xx-small", alpha=0.15, rotation=90),
+        )
+    ax.add_artist(text)
+
+def _get_cmap(cmap: Union[str, mpl.colors.ListedColormap]) -> mpl.colors.ListedColormap:
+    if isinstance(cmap, str):
+        cmap = mpl.colormaps[cmap]
+    elif isinstance(cmap, mpl.colors.ListedColormap):
+        cmap = cmap
+    else:
+        raise ValueError(f"Invalid cmap {cmap}")
+    return cmap
--- tests/plot/test_rose.py
+++ tests/plot/test_rose.py
@@ -21,9 +21,25 @@ def wave_data_model_obs():
 
 
 def test_rose(wave_data_model_obs):
-    df = wave_data_model_obs
-    ax = wind_rose(df, cbar_label="Hm0", mag_step=0.25, cmap1="jet")
+    data = wave_data_model_obs.to_numpy()
+    ax = wind_rose(data, mag_step=0.25, cmap1="jet")
     assert ax is not None
+
+
+def test_pretty_intervals_respects_defined_intervals():
+    data1 = np.array([0.1, 0.2, 0.3, 0.4, 0.5])
+    data2 = np.array([0.01, 0.2, 0.3, 0.4, 0.5, 0.6])
+    ui, vmin, vmax = pretty_intervals(
+        data_1=data1, data_2=data2, mag_bins=[0.1, 0.2, 0.45]
+    )
+    assert vmin == 0.1
+    assert vmax == 0.95  # TODO why?
+
+    # The computed intervals has one more than the mag_bins
+    assert list(ui[:-1]) == [0.1, 0.2, 0.45]
+
+    # The last interval is larger than the second last
+    assert ui[-1] > ui[-2]
 
 
 def test_pretty_intervals():
