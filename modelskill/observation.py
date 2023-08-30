709035872c7c76b4216670c9e2df093ccbf3506c
Henrik Andersson <jan@dhigroup.com>
2023-08-30 10:59:16 +0200

    Unused import

--- modelskill/observation.py
+++ modelskill/observation.py
@@ -7,7 +7,7 @@ Examples
 >>> o1 = PointObservation("klagshamn.dfs0", item=0, x=366844, y=6154291, name="Klagshamn")
 """
 import os
-from typing import Optional, Union
+from typing import Optional
 import numpy as np
 import pandas as pd
 import mikeio
