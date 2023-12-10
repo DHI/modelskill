9319770f239c9708cafb9cfed5785cae95b287fe
Henrik Andersson <jan@dhigroup.com>
2023-12-10 23:10:04 +0100

    Deprecation warning

--- modelskill/model/factory.py
+++ modelskill/model/factory.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
 from typing import Literal, Optional
+import warnings
 
 import pandas as pd
 import xarray as xr
@@ -46,7 +47,15 @@ def model_result(
     >>> ms.model_result("ERA5_DutchCoast.nc", item="swh", name="ERA5")
     <GridModelResult> 'ERA5'
     """
-    return ModelResult(data, gtype=gtype, **kwargs)  # type: ignore
+    if gtype is None:
+        geometry = _guess_gtype(data)
+    else:
+        geometry = GeometryType.from_string(gtype)
+
+    return _modelresult_lookup[geometry](
+        data=data,
+        **kwargs,
+    )
 
 
 class ModelResult:
@@ -57,8 +66,14 @@ class ModelResult:
         gtype: Optional[Literal["point", "track", "unstructured", "grid"]] = None,
         **kwargs,
     ):
+        # deprecated
+        warnings.warn(
+            FutureWarning(
+                "Use model_result or an explicit class instead, e.g. PointModelResult"
+            )
+        )
         if gtype is None:
-            geometry = cls._guess_gtype(data)
+            geometry = _guess_gtype(data)
         else:
             geometry = GeometryType.from_string(gtype)
 
@@ -67,45 +82,45 @@ class ModelResult:
             **kwargs,
         )
 
-    @staticmethod
-    def _guess_gtype(data) -> GeometryType:
-        if hasattr(data, "geometry"):
-            geom_str = repr(data.geometry).lower()
-            if "flex" in geom_str:
-                return GeometryType.UNSTRUCTURED
-            elif "point" in geom_str:
-                return GeometryType.POINT
-            else:
-                raise ValueError(
-                    "Could not guess gtype from geometry, please specify gtype, e.g. gtype='track'"
-                )
 
-        if isinstance(data, (str, Path)):
-            data = Path(data)
-            file_ext = data.suffix.lower()
-            if file_ext == ".dfsu":
-                return GeometryType.UNSTRUCTURED
-            elif file_ext == ".dfs0":
-                # could also be a track, but we don't know
-                return GeometryType.POINT
-            elif file_ext == ".nc":
-                # could also be point or track, but we don't know
-                return GeometryType.GRID
-            else:
-                raise ValueError(
-                    "Could not guess gtype from file extension, please specify gtype, e.g. gtype='track'"
-                )
+def _guess_gtype(data) -> GeometryType:
+    if hasattr(data, "geometry"):
+        geom_str = repr(data.geometry).lower()
+        if "flex" in geom_str:
+            return GeometryType.UNSTRUCTURED
+        elif "point" in geom_str:
+            return GeometryType.POINT
+        else:
+            raise ValueError(
+                "Could not guess gtype from geometry, please specify gtype, e.g. gtype='track'"
+            )
 
-        if isinstance(data, (xr.Dataset, xr.DataArray)):
-            if len(data.coords) >= 3:
-                # if DataArray use ndim instead
-                return GeometryType.GRID
-            else:
-                raise ValueError("Could not guess gtype from xarray object")
-        if isinstance(data, (pd.DataFrame, pd.Series)):
+    if isinstance(data, (str, Path)):
+        data = Path(data)
+        file_ext = data.suffix.lower()
+        if file_ext == ".dfsu":
+            return GeometryType.UNSTRUCTURED
+        elif file_ext == ".dfs0":
             # could also be a track, but we don't know
             return GeometryType.POINT
+        elif file_ext == ".nc":
+            # could also be point or track, but we don't know
+            return GeometryType.GRID
+        else:
+            raise ValueError(
+                "Could not guess gtype from file extension, please specify gtype, e.g. gtype='track'"
+            )
 
-        raise ValueError(
-            f"Geometry type (gtype) could not be guessed from this type of data: {type(data)}. Please specify gtype, e.g. gtype='track'"
-        )
+    if isinstance(data, (xr.Dataset, xr.DataArray)):
+        if len(data.coords) >= 3:
+            # if DataArray use ndim instead
+            return GeometryType.GRID
+        else:
+            raise ValueError("Could not guess gtype from xarray object")
+    if isinstance(data, (pd.DataFrame, pd.Series)):
+        # could also be a track, but we don't know
+        return GeometryType.POINT
+
+    raise ValueError(
+        f"Geometry type (gtype) could not be guessed from this type of data: {type(data)}. Please specify gtype, e.g. gtype='track'"
+    )
