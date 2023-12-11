ae77dc2d58b185647d5c418d23f1536b889b40e5
Henrik Andersson <jan@dhigroup.com>
2023-12-11 02:41:16 +0100

    Returns Comparer in case of single obs

--- tests/test_pointcompare.py
+++ tests/test_pointcompare.py
@@ -76,8 +76,8 @@ def test_skill_from_observation_with_missing_values(modelresult_oresund_WL):
         name="Klagshamn",
     )
     mr = modelresult_oresund_WL
-    cc = ms.match(o1, mr)
-    df = cc["Klagshamn"].skill().df
+    cmp = ms.match(o1, mr)
+    df = cmp.skill().df
     assert not np.any(np.isnan(df))
 
 
@@ -208,14 +208,14 @@ def test_skill_choose_metrics_back_defaults(cc):
 
 def test_obs_attrs_carried_over(klagshamn, modelresult_oresund_WL):
     klagshamn.data.attrs["A"] = "B"  # could also have been added in constructor
-    cmp = ms.match(klagshamn, modelresult_oresund_WL)[0]
+    cmp = ms.match(klagshamn, modelresult_oresund_WL)
     assert cmp.data.attrs["A"] == "B"
 
 
 def test_obs_aux_carried_over(klagshamn, modelresult_oresund_WL):
     klagshamn.data["aux"] = xr.ones_like(klagshamn.data["Klagshamn"])
     klagshamn.data["aux"].attrs["kind"] = "aux"
-    cmp = ms.match(klagshamn, modelresult_oresund_WL)[0]
+    cmp = ms.match(klagshamn, modelresult_oresund_WL)
     assert "aux" in cmp.data
     assert cmp.data["aux"].values[0] == 1.0
     assert cmp.data["aux"].attrs["kind"] == "aux"
@@ -223,7 +223,7 @@ def test_obs_aux_carried_over(klagshamn, modelresult_oresund_WL):
 
 
 def test_obs_aux_carried_over_nan(klagshamn, modelresult_oresund_WL):
-    cmp1 = ms.match(klagshamn, modelresult_oresund_WL)[0]
+    cmp1 = ms.match(klagshamn, modelresult_oresund_WL)
     assert cmp1.n_points == 71
     assert cmp1.time[0] == pd.Timestamp("2018-03-04 00:00:00")
     assert cmp1.data["Observation"].values[0] == pytest.approx(-0.11)
@@ -232,7 +232,7 @@ def test_obs_aux_carried_over_nan(klagshamn, modelresult_oresund_WL):
     klagshamn.data["aux"] = xr.ones_like(klagshamn.data["Klagshamn"])
     klagshamn.data["aux"].attrs["kind"] = "aux"
     klagshamn.data["aux"].loc["2018-03-04 00:00:00"] = np.nan
-    cmp2 = ms.match(klagshamn, modelresult_oresund_WL)[0]
+    cmp2 = ms.match(klagshamn, modelresult_oresund_WL)
     assert cmp2.n_points == 71
     assert cmp2.time[0] == pd.Timestamp("2018-03-04 00:00:00")
     assert cmp2.data["Observation"].values[0] == pytest.approx(-0.11)
@@ -242,7 +242,7 @@ def test_mod_aux_carried_over(klagshamn):
     mr = ms.ModelResult(
         "tests/testdata/Oresund2D_subset.dfsu", item=0, aux_items="U velocity"
     )
-    cmp = ms.match(klagshamn, mr)[0]
+    cmp = ms.match(klagshamn, mr)
     assert "U velocity" in cmp.data.data_vars
     assert cmp.data["U velocity"].values[0] == pytest.approx(-0.0360998)
     assert cmp.data["U velocity"].attrs["kind"] == "aux"
