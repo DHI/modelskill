c020237d1d9380278b231e357c065cfa17f161c6
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-12-18 01:34:36 +0100

    docstring

--- modelskill/configuration.py
+++ modelskill/configuration.py
@@ -18,7 +18,9 @@ def from_config(conf: Union[dict, str], *, validate_eum=True, relative_path=True
     validate_eum : bool, optional
         require eum to match, by default True
     relative_path: bool, optional
-            file path are relative to configuration file, and not current directory
+        True: file paths are relative to configuration file,
+        False: file paths are absolute (relative to the current directory),
+        by default True
 
     Returns
     -------
