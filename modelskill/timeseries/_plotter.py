cc4dfa04e9452e55904ca44385b2c5dcb67737a7
Henrik Andersson <jan@dhigroup.com>
2023-12-19 07:24:39 +0100

    Remove redunant plot()

--- modelskill/timeseries/_plotter.py
+++ modelskill/timeseries/_plotter.py
@@ -10,9 +10,6 @@ class TimeSeriesPlotter(Protocol):
     def __call__(self):
         pass
 
-    def plot(self):
-        pass
-
     def hist(self):
         pass
 
@@ -25,7 +22,7 @@ class MatplotlibTimeSeriesPlotter(TimeSeriesPlotter):
         self._ts = ts
 
     def __call__(self, title=None, color=None, marker=".", linestyle="None", **kwargs):
-        self.plot(
+        self.timeseries(
             title=title, color=color, marker=marker, linestyle=linestyle, **kwargs
         )
 
@@ -66,34 +63,6 @@ class MatplotlibTimeSeriesPlotter(TimeSeriesPlotter):
             ax.set_title(title)
         return ax
 
-    def plot(self, title=None, color=None, marker=".", linestyle="None", **kwargs):
-        """plot timeseries
-
-        Wraps pandas.DataFrame plot() method.
-
-        Parameters
-        ----------
-        title : str, optional
-            plot title, default: [name]
-        color : str, optional
-            plot color, by default '#d62728'
-        marker : str, optional
-            plot marker, by default '.'
-        linestyle : str, optional
-            line style, by default None
-        kwargs: other keyword arguments to df.plot()
-        """
-        kwargs["color"] = self._ts.color if color is None else color
-        ax = self._ts._values_as_series.plot(
-            marker=marker, linestyle=linestyle, **kwargs
-        )
-
-        title = self._ts.name if title is None else title
-        ax.set_title(title)
-
-        ax.set_ylabel(str(self._ts.quantity))
-        return ax
-
     def hist(self, bins=100, title=None, color=None, **kwargs):
         """plot histogram of timeseries values
 
@@ -128,12 +97,9 @@ class PlotlyTimeSeriesPlotter(TimeSeriesPlotter):
         self._ts = ts
 
     def __call__(self):
-        self.plot()
+        self.timeseries()
 
     def timeseries(self):
-        self.plot()
-
-    def plot(self):
         import plotly.express as px  # type: ignore
 
         fig = px.line(
