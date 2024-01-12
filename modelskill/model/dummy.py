db23871f9ec8f369461e717758320be18a4eae42
Henrik Andersson <jan@dhigroup.com>
2024-01-12 08:39:02 +0100

    Docstring

--- modelskill/model/dummy.py
+++ modelskill/model/dummy.py
@@ -18,7 +18,25 @@ class DummyModelResult:
     data : float, optional
         The value to return if strategy is 'constant', by default None
     name : str, optional
-        The name of the model result, by default "dummy"
+        The name of the model result, by default 'dummy'
+    strategy : str, optional
+        The strategy to use, 'mean' uses the mean of the observation, 'constant' uses the value given in data, by default 'constant'
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import modelskill as ms
+    >>> obs = ms.PointObservation(pd.DataFrame([0.0, 1.0], index=pd.date_range("2000", freq="H", periods=2)), name="foo")
+    >>> mr = ms.DummyModelResult(strategy='mean')
+    >>> pmr = mr.extract(obs)
+    >>> pmr.to_dataframe()
+                        dummy
+    time
+    2000-01-01 00:00:00    0.5
+    2000-01-01 01:00:00    0.5
+
+
+
     """
 
     def __init__(
