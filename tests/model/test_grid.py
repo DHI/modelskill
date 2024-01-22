70b3a5b3cd4dc3405d7887970e130646459d22ce
Henrik Andersson <jan@dhigroup.com>
2024-01-22 14:06:07 +0100

    End to end test

--- tests/model/test_grid.py
+++ tests/model/test_grid.py
@@ -224,13 +224,12 @@ def test_extract_point_from_3d():
     mr = ms.GridModelResult(
         "tests/testdata/cmems_mod_med_phy-sal_anfc_4.2km-3D_PT1H-m_1705916517624.nc",
         item="so",
+        name="MedSea",
     )
 
-    assert "x" in mr.data.dims  # renamed from longitude
-    assert "y" in mr.data.dims  # renamed from latitude
-    assert "z" in mr.data.dims  # renamed from depth
-
     point_ds = xr.open_dataset("tests/testdata/aegean_sea_salinity_ts.nc")
+
+    # TODO use x,y,z without explicitly setting them (NetCDF has them as coordinates)
     obs = ms.PointObservation(
         point_ds,
         x=float(point_ds.longitude),
@@ -238,7 +237,12 @@ def test_extract_point_from_3d():
         z=float(point_ds.depth),
         item="so",
     )
-    assert obs.z is not None
 
-    pmr = mr.extract(obs)
-    assert isinstance(pmr, ms.PointModelResult)
+    cmp = ms.match(obs=obs, mod=mr, spatial_method="nearest")
+    assert cmp.quantity.name == "Salinity"
+    assert cmp.quantity.unit == "0.001"
+
+    sc = cmp.score()
+
+    # "Observed" data is extracted from the 3D model result, so the score should be 0.0
+    assert sc["MedSea"] == pytest.approx(0.0)
