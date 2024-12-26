6d9d4ee77b4504383fc7a0b223f138a752bbc257
jsmariegaard <jem@dhigroup.com>
2024-12-26 14:58:10 +0100

    Add oresund dataset

--- modelskill/data.py
+++ modelskill/data.py
@@ -2,5 +2,24 @@ import modelskill as ms
 
 
 def vistula():
+    """10-year discharge data for Vistula catchment, Poland
+
+    Returns
+    -------
+    ComparerCollection
+        _description_
+    """
     fn = "./data/vistula.msk"
+    return ms.load(fn)
+
+
+def oresund():
+    """Oresund water level data for 2022
+
+    Returns
+    -------
+    ComparerCollection
+        _description_
+    """
+    fn = "./data/oresund.msk"
     return ms.load(fn)
--- tests/test_data.py
+++ tests/test_data.py
@@ -3,4 +3,9 @@ import modelskill as ms
 
 def test_load_vistula():
     cc = ms.data.vistula()
-    assert cc is not None
+    assert isinstance(cc, ms.ComparerCollection)
+
+
+def test_load_oresund():
+    cc = ms.data.oresund()
+    assert isinstance(cc, ms.ComparerCollection)
