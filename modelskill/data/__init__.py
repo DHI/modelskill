744233ed792b98d71806430d0b703402c569e5f0
Henrik Andersson <jan@dhigroup.com>
2025-01-02 11:14:43 +0100

    Make sure data files are part of the package.

--- modelskill/data/__init__.py
+++ modelskill/data/__init__.py
@@ -1,25 +1,27 @@
+from importlib.resources import files
+
 import modelskill as ms
+from ..comparison import ComparerCollection
 
 
-def vistula():
+def vistula() -> ComparerCollection:
     """10-year discharge data for Vistula catchment, Poland
 
     Returns
     -------
     ComparerCollection
-        _description_
     """
-    fn = "./data/vistula.msk"
+
+    fn = str(files("modelskill.data") / "vistula.msk")
     return ms.load(fn)
 
 
-def oresund():
+def oresund() -> ComparerCollection:
     """Oresund water level data for 2022
 
     Returns
     -------
     ComparerCollection
-        _description_
     """
-    fn = "./data/oresund.msk"
+    fn = str(files("modelskill.data") / "oresund.msk")
     return ms.load(fn)
