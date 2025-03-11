89d057017512df716359fbd1d94f5c5087f2f430
Henrik Andersson <jan@dhigroup.com>
2025-03-11 12:13:43 +0100

    Good enough

--- modelskill/timeseries/_point.py
+++ modelskill/timeseries/_point.py
@@ -45,7 +45,6 @@ def _parse_point_items(
 
     # check that there are no duplicates
     res = PointItem(values=item, aux=aux_items_str)
-    # TODO move validation to PointItem
     if len(set(res.all)) != len(res.all):
         raise ValueError(f"Duplicate items! {res.all}")
 
@@ -67,16 +66,13 @@ def _parse_point_input(
         data, get_args(PointType)
     ), f"Could not construct object from provided data of type {type(data)}"
 
-    # TODO refactor and create separate methods for each input type
     if isinstance(data, (str, Path)):
         suffix = Path(data).suffix
         if suffix == ".dfs0":
             name = name or Path(data).stem
-            # TODO avoid changing type of data
             data = mikeio.read(data)  # now mikeio.Dataset
         elif suffix == ".nc":
             stem = Path(data).stem
-            # TODO avoid changing type of data
             data = xr.open_dataset(data)
             name = name or data.attrs.get("name") or stem
     elif isinstance(data, mikeio.Dfs0):
