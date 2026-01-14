59b2104c0c7197166ef61383edc4d437e6668bd3
jpalm3r <rp.jaume@gmail.com>
2026-01-14 15:37:21 +0100

    Adding docstring

--- src/modelskill/model/network.py
+++ src/modelskill/model/network.py
@@ -9,10 +9,36 @@ from .point import PointModelResult
 
 
 class NetworkModelResult(PointModelResult):
+    """Model result for a network location.
+
+    Construct a NetworkModelResult from a res1d data source.
+
+    Parameters
+    ----------
+    data : str, Path or mikeio1d.Res1D
+        filename (.res1d) or object with the data
+    quantity : str
+        The name of the model result,
+        by default None (will be set to file name or item name)
+    reach : str, optional
+        Reach id in the network
+    node : int, optional
+        Node id in the network
+    chainage : float, optional
+        Chainage number in its respective reach
+    gridpoint : int, optional
+        Index associated to the gridpoints in the reach
+    name : Optional[str], optional
+        The name of the model result,
+        by default None (will be set to file name or item name)
+    aux_items : Optional[list[int | str]], optional
+        Auxiliary items, by default None
+    """
+
     def __init__(
         self,
         data: Res1D | str,
-        quantity: Optional[str | Quantity] = None,
+        quantity: str | Quantity,
         *,
         reach: Optional[str] = None,
         node: Optional[int] = None,
