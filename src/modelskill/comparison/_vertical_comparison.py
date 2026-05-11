50a4c2e262ad2c5609e91e8c4c841c7d765dbe94
Lars Jonasson <lajo@dhigroup.com>
2026-05-11 15:38:37 +0200

    unused import

--- src/modelskill/comparison/_vertical_comparison.py
+++ src/modelskill/comparison/_vertical_comparison.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import matplotlib
 import numpy as np
-import pandas as pd
 from typing import TYPE_CHECKING, Callable, Iterable, Sequence, Tuple
 from ..types import GeometryType
 
